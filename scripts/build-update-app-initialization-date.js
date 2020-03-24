014b8442518e2fdceb296109e1c923986080d362
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-03-24 19:56:04 +0100

    feat(command-version): add new date for release and enhance the UI
    rename the release date to initialization date which is more accurate
    add a new release date (set to
    do with github)
    change the UI to display the content of multiple columns
    add more logs to debug the
    messages
    fix a parsing moment error due to unknown dates

--- scripts/build-update-app-initialization-date.js
+++ scripts/build-update-app-initialization-date.js
@@ -5,15 +5,15 @@ const CHALK = require(`./chalk`);
 const LOGGER = require(`./logger`);
 const APP_ROOT_PATH = require(`app-root-path`);
 
-const CONTEXT = `build-update-app-release-date`;
+const CONTEXT = `build-update-app-initialization-date`;
 
-function updateEnvironmentAppReleaseDate(environment) {
+function updateEnvironmentAppInitializationDate(environment) {
   if (!_.has(environment, `app`)) {
     environment.app = {};
   }
 
   // eslint-disable-next-line new-cap
-  environment.app.releaseDate = MOMENT().format();
+  environment.app.initializationDate = MOMENT().format();
 
   return environment;
 }
@@ -21,14 +21,14 @@ function updateEnvironmentAppReleaseDate(environment) {
 LOGGER.debug(CONTEXT, CHALK.chalkText(`Read environment file from dist folder...`));
 
 FS.readJson(`${APP_ROOT_PATH.path}/dist/environment.json`).then((environment) => {
-  const updatedEnvironment = updateEnvironmentAppReleaseDate(environment);
+  const updatedEnvironment = updateEnvironmentAppInitializationDate(environment);
 
-  LOGGER.debug(CONTEXT, CHALK.chalkText(`Update environment app release date inside dist folder...`));
+  LOGGER.debug(CONTEXT, CHALK.chalkText(`Update environment app initialization date inside dist folder...`));
 
   FS.writeJson(`${APP_ROOT_PATH.path}/dist/environment.json`, updatedEnvironment).then(() => {
-    LOGGER.success(CONTEXT, CHALK.chalkText(`Environment app release date successfully updated inside dist folder`));
+    LOGGER.success(CONTEXT, CHALK.chalkText(`Environment app initialization date successfully updated inside dist folder`));
   }).catch((error) => {
-    LOGGER.error(CONTEXT, CHALK.chalkText(`Failed to update environment app release date inside dist folder`));
+    LOGGER.error(CONTEXT, CHALK.chalkText(`Failed to update environment app initialization date inside dist folder`));
     LOGGER.error(CONTEXT, CHALK.chalkText(error));
   });
 }).catch((error) => {
