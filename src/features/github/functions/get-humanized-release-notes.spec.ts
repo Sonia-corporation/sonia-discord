855b0b01725faada2d0a59eec9611f0fc2132b13
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-03-24 22:27:31 +0100

    build(test): add jest
    add jest configuration
    add a linter for jest (very strict)
    add majestic to test in the browser

--- src/features/github/functions/get-humanized-release-notes.spec.ts
+++ src/features/github/functions/get-humanized-release-notes.spec.ts
@@ -1,9 +1,19 @@
-describe('', (): void => {
-  beforeEach((): void => {
+import { getHumanizedReleaseNotes } from './get-humanized-release-notes';
 
-  });
+describe(`getHumanizedReleaseNotes()`, (): void => {
+  let notes: string;
 
-  it('', (): void => {
+  describe(`when the given release notes is a simple string`, (): void => {
+    beforeEach((): void => {
+      notes = `dummy notes`;
+    });
 
+    it(`should not change the given release notes`, (): void => {
+      expect.assertions(1);
+
+      const result = getHumanizedReleaseNotes(notes);
+
+      expect(result).toStrictEqual(`dummy notes`);
+    });
   });
 });
