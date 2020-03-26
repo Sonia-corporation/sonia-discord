5bb44a82514acdcfff8f024af0b98a6a34a54455
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-03-26 19:29:14 +0100

    build(prod): add the scout apm configuration
    though this is disable for now due to an issue on windows

--- src/features/server/services/server-service.ts
+++ src/features/server/services/server-service.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import _ from 'lodash';
+import { AppConfigService } from '../../app/services/app-config-service';
 import { ChalkService } from '../../logger/services/chalk-service';
 import { LoggerService } from '../../logger/services/logger-service';
 
@@ -16,11 +17,17 @@ export class ServerService {
 
   private readonly _loggerService = LoggerService.getInstance();
   private readonly _chalkService = ChalkService.getInstance();
+  private readonly _appConfigService = AppConfigService.getInstance();
   private readonly _port = 3001;
   private readonly _app = express();
   private readonly _className = `ServerService`;
 
   public constructor() {
+    this._initializeApp();
+  }
+
+  private _initializeApp(): void {
+    this._createScoutMiddleware();
     this._listen();
   }
 
@@ -31,5 +38,18 @@ export class ServerService {
         message: this._chalkService.text(`listening on port: ${this._chalkService.value(this._port)}`)
       });
     });
+  }
+
+  private _createScoutMiddleware(): void {
+    if (this._appConfigService.isProduction()) {
+      /**
+       * @description
+       * Not compatible with Windows for now
+       * Could not find a way to properly handle errors to run it on production only
+       * Issue: https://github.com/scoutapp/scout_apm_node/issues/187
+       *
+       * this._app.use(scout.expressMiddleware());
+       */
+    }
   }
 }
