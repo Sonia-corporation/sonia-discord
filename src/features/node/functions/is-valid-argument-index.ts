d367c9c80b650fd19181b58b2728ba856965f824
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-03-26 16:27:39 +0100

    refactor(get-node-argument): split into multiple functions

---
+++ src/features/node/functions/is-valid-argument-index.ts
@@ -0,0 +1,5 @@
+import _ from 'lodash';
+
+export function isValidArgumentIndex(argumentIndex: Readonly<number>): argumentIndex is number {
+  return _.isFinite(argumentIndex) && _.gte(argumentIndex, 0);
+}
