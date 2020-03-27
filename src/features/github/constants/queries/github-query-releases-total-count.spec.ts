e8eb044a58b6ab147c8a40fabc62f86c4559d36f
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-03-27 20:17:05 +0100

    test(github): add coverage for the const GITHUB_QUERY_RELEASES_TOTAL_COUNT

---
+++ src/features/github/constants/queries/github-query-releases-total-count.spec.ts
@@ -0,0 +1,13 @@
+import { GITHUB_QUERY_RELEASES_TOTAL_COUNT } from './github-query-releases-total-count';
+
+describe(`GITHUB_QUERY_RELEASES_TOTAL_COUNT`, (): void => {
+  it(`should contains the query used to have the GitHub total release count`, (): void => {
+    expect.assertions(1);
+
+    expect(GITHUB_QUERY_RELEASES_TOTAL_COUNT).toStrictEqual(`{repository(owner: "Sonia-corporation", name: "il-est-midi-discord") {
+  releases {
+    totalCount
+  }
+}}`);
+  });
+});
