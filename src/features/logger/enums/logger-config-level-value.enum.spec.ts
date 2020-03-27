859931df4764b470f7a2cfb381d32bfdbbc8efb8
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-03-27 12:51:51 +0100

    test(enum): add coverage for LoggerConfigLevelValueEnum

---
+++ src/features/logger/enums/logger-config-level-value.enum.spec.ts
@@ -0,0 +1,33 @@
+import { LoggerConfigLevelValueEnum } from './logger-config-level-value.enum';
+
+describe(`loggerConfigLevelValueEnum`, (): void => {
+  it(`should have a member "error"`, (): void => {
+    expect.assertions(1);
+
+    expect(LoggerConfigLevelValueEnum.error).toStrictEqual(0);
+  });
+
+  it(`should have a member "warning"`, (): void => {
+    expect.assertions(1);
+
+    expect(LoggerConfigLevelValueEnum.warning).toStrictEqual(1);
+  });
+
+  it(`should have a member "success"`, (): void => {
+    expect.assertions(1);
+
+    expect(LoggerConfigLevelValueEnum.success).toStrictEqual(2);
+  });
+
+  it(`should have a member "log"`, (): void => {
+    expect.assertions(1);
+
+    expect(LoggerConfigLevelValueEnum.log).toStrictEqual(3);
+  });
+
+  it(`should have a member "debug"`, (): void => {
+    expect.assertions(1);
+
+    expect(LoggerConfigLevelValueEnum.debug).toStrictEqual(4);
+  });
+});
