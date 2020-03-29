507727a4be499e52a5022ab356afe5b6f48c9269
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-03-29 12:10:01 +0200

    refactor(node-argument): create a new function to split the code

--- src/features/node/functions/get-node-argument.ts
+++ src/features/node/functions/get-node-argument.ts
@@ -1,20 +1,12 @@
-import _ from 'lodash';
 import { getArgumentIndex } from './get-argument-index';
+import { getNodeArgumentByIndex } from './get-node-argument-by-index';
 import { hasCommandLineArguments } from './has-command-line-arguments';
-import { isExistingArgument } from './is-existing-argument';
-import { isValidArgumentIndex } from './is-valid-argument-index';
 
 export function getNodeArgument(name: Readonly<string>): unknown | null {
   if (hasCommandLineArguments()) {
     const argumentIndex: number = getArgumentIndex(name);
 
-    if (isValidArgumentIndex(argumentIndex)) {
-      const originArgumentValueIndex: number = _.add(argumentIndex, 1);
-
-      if (isExistingArgument(originArgumentValueIndex)) {
-        return process.argv[ originArgumentValueIndex ];
-      }
-    }
+    return getNodeArgumentByIndex(argumentIndex);
   }
 
   return null;
