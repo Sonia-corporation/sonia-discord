57f0730ab775f4381b5763c6a21e1c04a4b74981
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-04-05 11:09:53 +0200

    test(coverage): add coverage for isValidPort function

--- src/features/node/functions/is-valid-port.ts
+++ src/features/node/functions/is-valid-port.ts
@@ -2,6 +2,7 @@ import _ from "lodash";
 
 export function isValidPort(port: unknown): port is string | number {
   return (
-    !_.isNil(port) && !_.isEmpty(port) && (_.isString(port) || _.isNumber(port))
+    !_.isNil(port) &&
+    ((_.isString(port) && !_.isEmpty(port)) || _.isNumber(port))
   );
 }
