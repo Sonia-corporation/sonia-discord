406a8437b0884ddb6b599843e5a26c672cd6ba48
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-04-05 13:02:12 +0200

    refactor(node): enhance the type checks

--- src/features/node/functions/get-environment-port.ts
+++ src/features/node/functions/get-environment-port.ts
@@ -5,7 +5,11 @@ export function getEnvironmentPort(): number | null {
   const port: string | undefined = process.env.PORT;
 
   if (isValidPort(port)) {
-    return _.parseInt(port, 10);
+    if (_.isString(port)) {
+      return _.parseInt(port, 10);
+    }
+
+    return port;
   }
 
   return null;
--- src/features/node/functions/is-valid-port.ts
+++ src/features/node/functions/is-valid-port.ts
@@ -1,8 +1,14 @@
 import _ from "lodash";
 
+export function isValidPort(port: undefined): false;
+export function isValidPort(port: null): false;
+export function isValidPort(port: boolean): false;
+export function isValidPort(port: string): port is string;
+export function isValidPort(port: number): port is number;
+export function isValidPort(port: unknown): port is string | number;
 export function isValidPort(port: unknown): port is string | number {
   return (
     !_.isNil(port) &&
-    ((_.isString(port) && !_.isEmpty(port)) || _.isNumber(port))
+    ((_.isString(port) && !_.isEmpty(port)) || _.isFinite(port))
   );
 }
