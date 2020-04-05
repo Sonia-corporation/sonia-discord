2513b7ae4ec8a8f3b6f0bbcea6fbcca009798b99
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-04-05 13:10:34 +0200

    test(coverage): add coverage for hasCommandLineArguments function

---
+++ src/features/node/functions/has-command-line-arguments.spec.ts
@@ -0,0 +1,89 @@
+import { hasCommandLineArguments } from "./has-command-line-arguments";
+
+describe(`hasCommandLineArguments()`, (): void => {
+  describe(`when process is undefined`, (): void => {
+    beforeEach((): void => {
+      (process as unknown) = undefined;
+    });
+
+    it(`should return false`, (): void => {
+      expect.assertions(1);
+
+      const result = hasCommandLineArguments();
+
+      expect(result).toStrictEqual(false);
+    });
+  });
+
+  describe(`when process is null`, (): void => {
+    beforeEach((): void => {
+      (process as unknown) = null;
+    });
+
+    it(`should return false`, (): void => {
+      expect.assertions(1);
+
+      const result = hasCommandLineArguments();
+
+      expect(result).toStrictEqual(false);
+    });
+  });
+
+  describe(`when process is valid`, (): void => {
+    describe(`when process argv is undefined`, (): void => {
+      beforeEach((): void => {
+        (process.argv as unknown) = undefined;
+      });
+
+      it(`should return false`, (): void => {
+        expect.assertions(1);
+
+        const result = hasCommandLineArguments();
+
+        expect(result).toStrictEqual(false);
+      });
+    });
+
+    describe(`when process argv is null`, (): void => {
+      beforeEach((): void => {
+        (process.argv as unknown) = null;
+      });
+
+      it(`should return false`, (): void => {
+        expect.assertions(1);
+
+        const result = hasCommandLineArguments();
+
+        expect(result).toStrictEqual(false);
+      });
+    });
+
+    describe(`when process argv is an empty array`, (): void => {
+      beforeEach((): void => {
+        process.argv = [];
+      });
+
+      it(`should return true`, (): void => {
+        expect.assertions(1);
+
+        const result = hasCommandLineArguments();
+
+        expect(result).toStrictEqual(true);
+      });
+    });
+
+    describe(`when process argv is an array of string`, (): void => {
+      beforeEach((): void => {
+        process.argv = [`dummy`];
+      });
+
+      it(`should return true`, (): void => {
+        expect.assertions(1);
+
+        const result = hasCommandLineArguments();
+
+        expect(result).toStrictEqual(true);
+      });
+    });
+  });
+});
