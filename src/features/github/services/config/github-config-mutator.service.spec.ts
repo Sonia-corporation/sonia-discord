cc1eaf24da1e010894199b5f5ed4c4cfff4eed70
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-04-25 18:54:14 +0200

    test(github-config-services): add more coverage

--- src/features/github/services/config/github-config-mutator.service.spec.ts
+++ src/features/github/services/config/github-config-mutator.service.spec.ts
@@ -1,9 +1,13 @@
+import { ServiceNameEnum } from "../../../../enums/service-name.enum";
 import { PartialNested } from "../../../../types/partial-nested";
 import { IConfigUpdateString } from "../../../config/interfaces/config-update-string";
 import { ConfigService } from "../../../config/services/config.service";
+import { CoreEventService } from "../../../core/services/core-event.service";
+import { LoggerService } from "../../../logger/services/logger.service";
 import { IGithubConfig } from "../../interfaces/github-config";
 import { GithubConfigCoreService } from "./github-config-core.service";
 import { GithubConfigMutatorService } from "./github-config-mutator.service";
+import { GithubConfigService } from "./github-config.service";
 
 jest.mock(`../../../config/services/config.service`);
 
@@ -11,17 +15,175 @@ describe(`GithubConfigMutatorService`, (): void => {
   let service: GithubConfigMutatorService;
   let configService: ConfigService;
   let githubConfigCoreService: GithubConfigCoreService;
+  let coreEventService: CoreEventService;
 
   beforeEach((): void => {
-    service = GithubConfigMutatorService.getInstance();
     configService = ConfigService.getInstance();
     githubConfigCoreService = GithubConfigCoreService.getInstance();
+    coreEventService = CoreEventService.getInstance();
+  });
+
+  describe(`getInstance()`, (): void => {
+    let config: PartialNested<IGithubConfig> | undefined;
+
+    beforeEach((): void => {
+      config = {
+        bugReportUrl: `dummy-bug-report-url`,
+        personalAccessToken: `dummy-personal-access-token`,
+      };
+    });
+
+    it(`should create a GithubConfigMutator service`, (): void => {
+      expect.assertions(1);
+
+      service = GithubConfigMutatorService.getInstance(config);
+
+      expect(service).toStrictEqual(expect.any(GithubConfigMutatorService));
+    });
+
+    it(`should return the created GithubConfigMutator service`, (): void => {
+      expect.assertions(1);
+
+      const result = GithubConfigMutatorService.getInstance();
+
+      expect(result).toStrictEqual(service);
+    });
+  });
+
+  describe(`constructor()`, (): void => {
+    let config: PartialNested<IGithubConfig> | undefined;
+
+    let coreEventServiceNotifyServiceCreatedSpy: jest.SpyInstance;
+
+    beforeEach((): void => {
+      coreEventServiceNotifyServiceCreatedSpy = jest
+        .spyOn(coreEventService, `notifyServiceCreated`)
+        .mockImplementation();
+    });
+
+    it(`should notify the GithubConfigMutator service creation`, (): void => {
+      expect.assertions(2);
+
+      service = new GithubConfigMutatorService();
+
+      expect(coreEventServiceNotifyServiceCreatedSpy).toHaveBeenCalledTimes(1);
+      expect(coreEventServiceNotifyServiceCreatedSpy).toHaveBeenCalledWith(
+        ServiceNameEnum.GITHUB_CONFIG_MUTATOR_SERVICE
+      );
+    });
+
+    describe(`when the given config is undefined`, (): void => {
+      beforeEach((): void => {
+        config = undefined;
+      });
+
+      it(`should not update the current bug report url`, (): void => {
+        expect.assertions(1);
+        githubConfigCoreService.bugReportUrl = `bugReportUrl`;
+
+        service = new GithubConfigMutatorService(config);
+
+        expect(githubConfigCoreService.bugReportUrl).toStrictEqual(
+          `bugReportUrl`
+        );
+      });
+
+      it(`should not update the current personal access token`, (): void => {
+        expect.assertions(1);
+        githubConfigCoreService.personalAccessToken = `personalAccessToken`;
+
+        service = new GithubConfigMutatorService(config);
+
+        expect(githubConfigCoreService.personalAccessToken).toStrictEqual(
+          `personalAccessToken`
+        );
+      });
+    });
+
+    describe(`when the given config is a complete object`, (): void => {
+      beforeEach((): void => {
+        config = {
+          bugReportUrl: `dummy-bug-report-url`,
+          personalAccessToken: `dummy-personal-access-token`,
+        };
+      });
+
+      it(`should override the bug report url`, (): void => {
+        expect.assertions(1);
+        githubConfigCoreService.bugReportUrl = `bugReportUrl`;
+
+        service = new GithubConfigMutatorService(config);
+
+        expect(githubConfigCoreService.bugReportUrl).toStrictEqual(
+          `dummy-bug-report-url`
+        );
+      });
+
+      it(`should override the personal access token`, (): void => {
+        expect.assertions(1);
+        githubConfigCoreService.personalAccessToken = `personalAccessToken`;
+
+        service = new GithubConfigMutatorService(config);
+
+        expect(githubConfigCoreService.personalAccessToken).toStrictEqual(
+          `dummy-personal-access-token`
+        );
+      });
+    });
+  });
+
+  describe(`preUpdateConfig()`, (): void => {
+    let loggerServiceGetInstanceSpy: jest.SpyInstance;
+    let githubConfigCoreServiceGetInstanceSpy: jest.SpyInstance;
+    let githubConfigServiceGetInstanceSpy: jest.SpyInstance;
+
+    beforeEach((): void => {
+      service = GithubConfigMutatorService.getInstance();
+
+      loggerServiceGetInstanceSpy = jest.spyOn(LoggerService, `getInstance`);
+      githubConfigCoreServiceGetInstanceSpy = jest.spyOn(
+        GithubConfigCoreService,
+        `getInstance`
+      );
+      githubConfigServiceGetInstanceSpy = jest.spyOn(
+        GithubConfigService,
+        `getInstance`
+      );
+    });
+
+    it(`should create the Logger service instance`, (): void => {
+      expect.assertions(2);
+
+      service.preUpdateConfig();
+
+      expect(loggerServiceGetInstanceSpy).toHaveBeenCalledTimes(1);
+      expect(loggerServiceGetInstanceSpy).toHaveBeenCalledWith();
+    });
+
+    it(`should create the GithubConfigCore service instance`, (): void => {
+      expect.assertions(2);
+
+      service.preUpdateConfig();
+
+      expect(githubConfigCoreServiceGetInstanceSpy).toHaveBeenCalledTimes(1);
+      expect(githubConfigCoreServiceGetInstanceSpy).toHaveBeenCalledWith();
+    });
+
+    it(`should create the GithubConfig service instance`, (): void => {
+      expect.assertions(2);
+
+      service.preUpdateConfig();
+
+      expect(githubConfigServiceGetInstanceSpy).toHaveBeenCalledTimes(1);
+      expect(githubConfigServiceGetInstanceSpy).toHaveBeenCalledWith();
+    });
   });
 
   describe(`updateConfig()`, (): void => {
     let config: PartialNested<IGithubConfig> | undefined;
 
     beforeEach((): void => {
+      service = GithubConfigMutatorService.getInstance();
       githubConfigCoreService.bugReportUrl = `dummy-bug-report-url`;
       githubConfigCoreService.personalAccessToken = `dummy-personal-access-token`;
     });
@@ -88,6 +250,7 @@ describe(`GithubConfigMutatorService`, (): void => {
     let configServiceGetUpdatedStringSpy: jest.SpyInstance;
 
     beforeEach((): void => {
+      service = GithubConfigMutatorService.getInstance();
       bugReportUrl = `bug-report-url`;
       githubConfigCoreService.bugReportUrl = `dummy-bug-report-url`;
 
@@ -127,6 +290,7 @@ describe(`GithubConfigMutatorService`, (): void => {
     let configServiceGetUpdatedStringSpy: jest.SpyInstance;
 
     beforeEach((): void => {
+      service = GithubConfigMutatorService.getInstance();
       personalAccessToken = `personal-access-token`;
       githubConfigCoreService.personalAccessToken = `dummy-personal-access-token`;
 
--- src/features/github/services/config/github-config.service.spec.ts
+++ src/features/github/services/config/github-config.service.spec.ts
@@ -1,3 +1,5 @@
+import { ServiceNameEnum } from "../../../../enums/service-name.enum";
+import { CoreEventService } from "../../../core/services/core-event.service";
 import { IGithubConfig } from "../../interfaces/github-config";
 import { GithubConfigCoreService } from "./github-config-core.service";
 import { GithubConfigService } from "./github-config.service";
@@ -7,14 +9,55 @@ jest.mock(`../../../config/services/config.service`);
 describe(`GithubConfigService`, (): void => {
   let service: GithubConfigService;
   let githubConfigCoreService: GithubConfigCoreService;
+  let coreEventService: CoreEventService;
 
   beforeEach((): void => {
-    service = GithubConfigService.getInstance();
     githubConfigCoreService = GithubConfigCoreService.getInstance();
+    coreEventService = CoreEventService.getInstance();
+  });
+
+  describe(`getInstance()`, (): void => {
+    it(`should create a GithubConfig service`, (): void => {
+      expect.assertions(1);
+
+      service = GithubConfigService.getInstance();
+
+      expect(service).toStrictEqual(expect.any(GithubConfigService));
+    });
+
+    it(`should return the created GithubConfig service`, (): void => {
+      expect.assertions(1);
+
+      const result = GithubConfigService.getInstance();
+
+      expect(result).toStrictEqual(service);
+    });
+  });
+
+  describe(`constructor()`, (): void => {
+    let coreEventServiceNotifyServiceCreatedSpy: jest.SpyInstance;
+
+    beforeEach((): void => {
+      coreEventServiceNotifyServiceCreatedSpy = jest
+        .spyOn(coreEventService, `notifyServiceCreated`)
+        .mockImplementation();
+    });
+
+    it(`should notify the GithubConfig service creation`, (): void => {
+      expect.assertions(2);
+
+      service = new GithubConfigService();
+
+      expect(coreEventServiceNotifyServiceCreatedSpy).toHaveBeenCalledTimes(1);
+      expect(coreEventServiceNotifyServiceCreatedSpy).toHaveBeenCalledWith(
+        ServiceNameEnum.GITHUB_CONFIG_SERVICE
+      );
+    });
   });
 
   describe(`getConfig()`, (): void => {
     beforeEach((): void => {
+      service = GithubConfigService.getInstance();
       githubConfigCoreService.bugReportUrl = `dummy-bug-report-url`;
       githubConfigCoreService.personalAccessToken = `dummy-personal-access-token`;
     });
@@ -33,6 +76,7 @@ describe(`GithubConfigService`, (): void => {
 
   describe(`getBugReportUrl()`, (): void => {
     beforeEach((): void => {
+      service = GithubConfigService.getInstance();
       githubConfigCoreService.bugReportUrl = `dummy-bug-report-url`;
     });
 
@@ -47,6 +91,7 @@ describe(`GithubConfigService`, (): void => {
 
   describe(`getPersonalAccessToken()`, (): void => {
     beforeEach((): void => {
+      service = GithubConfigService.getInstance();
       githubConfigCoreService.personalAccessToken = `dummy-personal-access-token`;
     });
 
