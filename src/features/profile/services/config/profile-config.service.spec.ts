9ea6e4ba2f9de93a2cd17e8538a6cfff1f1b2729
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-04-25 16:46:12 +0200

    test(profile-config-services): add more coverage

--- src/features/profile/services/config/profile-config.service.spec.ts
+++ src/features/profile/services/config/profile-config.service.spec.ts
@@ -1,3 +1,5 @@
+import { ServiceNameEnum } from "../../../../enums/service-name.enum";
+import { CoreEventService } from "../../../core/services/core-event.service";
 import { IProfileConfig } from "../../interfaces/profile-config";
 import { ProfileConfigCoreService } from "./profile-config-core.service";
 import { ProfileConfigService } from "./profile-config.service";
@@ -5,14 +7,55 @@ import { ProfileConfigService } from "./profile-config.service";
 describe(`ProfileConfigService`, (): void => {
   let service: ProfileConfigService;
   let profileConfigCoreService: ProfileConfigCoreService;
+  let coreEventService: CoreEventService;
 
   beforeEach((): void => {
-    service = ProfileConfigService.getInstance();
     profileConfigCoreService = ProfileConfigCoreService.getInstance();
+    coreEventService = CoreEventService.getInstance();
+  });
+
+  describe(`getInstance()`, (): void => {
+    it(`should create a ProfileConfig service`, (): void => {
+      expect.assertions(1);
+
+      service = ProfileConfigService.getInstance();
+
+      expect(service).toStrictEqual(expect.any(ProfileConfigService));
+    });
+
+    it(`should return the created ProfileConfig service`, (): void => {
+      expect.assertions(1);
+
+      const result = ProfileConfigService.getInstance();
+
+      expect(result).toStrictEqual(service);
+    });
+  });
+
+  describe(`constructor()`, (): void => {
+    let coreEventServiceNotifyServiceCreatedSpy: jest.SpyInstance;
+
+    beforeEach((): void => {
+      coreEventServiceNotifyServiceCreatedSpy = jest
+        .spyOn(coreEventService, `notifyServiceCreated`)
+        .mockImplementation();
+    });
+
+    it(`should notify the ProfileConfig service creation`, (): void => {
+      expect.assertions(2);
+
+      service = new ProfileConfigService();
+
+      expect(coreEventServiceNotifyServiceCreatedSpy).toHaveBeenCalledTimes(1);
+      expect(coreEventServiceNotifyServiceCreatedSpy).toHaveBeenCalledWith(
+        ServiceNameEnum.PROFILE_CONFIG_SERVICE
+      );
+    });
   });
 
   describe(`getConfig()`, (): void => {
     beforeEach((): void => {
+      service = ProfileConfigService.getInstance();
       profileConfigCoreService.nickname = `dummy-nickname`;
     });
 
@@ -29,6 +72,7 @@ describe(`ProfileConfigService`, (): void => {
 
   describe(`getNickname()`, (): void => {
     beforeEach((): void => {
+      service = ProfileConfigService.getInstance();
       profileConfigCoreService.nickname = `dummy-nickname`;
     });
 
