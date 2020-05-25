7853c17a846828de0aba8a26e3ef33d8a507bac6
Geoffrey "C0ZEN" TESTELIN <geoffrey.testelin@gmail.com>
2020-05-25 23:27:09 +0200

    refactor(timezone): add enum for timezone

---
+++ src/features/time/enums/timezone.enum.spec.ts
@@ -0,0 +1,9 @@
+import { TimezoneEnum } from "./timezone.enum";
+
+describe("TimezoneEnum", (): void => {
+  it("", (): void => {
+    expect.assertions(1);
+
+    expect(TimezoneEnum.PARIS).toStrictEqual("Europe/Paris");
+  });
+});
