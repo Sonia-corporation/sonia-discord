3560075f2afc9755865fd49030457a94d09bf986
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-07-16 11:29:48 +0200

    test(discord): add more coverage

--- src/features/discord/activities/enums/discord-activity-name.enum.spec.ts
+++ src/features/discord/activities/enums/discord-activity-name.enum.spec.ts
@@ -1,35 +1,233 @@
-import { DiscordActivityTypeEnum } from "./discord-activity-type.enum";
+import { DiscordActivityNameEnum } from "./discord-activity-name.enum";
 
-describe(`DiscordActivityTypeEnum`, (): void => {
-  it(`should have a member "CUSTOM_STATUS"`, (): void => {
+describe(`DiscordActivityNameEnum`, (): void => {
+  it(`should have a member "SPOTIFY"`, (): void => {
     expect.assertions(1);
 
-    expect(DiscordActivityTypeEnum.CUSTOM_STATUS).toStrictEqual(
-      `CUSTOM_STATUS`
+    expect(DiscordActivityNameEnum.SPOTIFY).toStrictEqual(`Spotify`);
+  });
+
+  it(`should have a member "MOM"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.MOM).toStrictEqual(`mom`);
+  });
+
+  it(`should have a member "MOMMY"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.MOMMY).toStrictEqual(`mommy`);
+  });
+
+  it(`should have a member "DAD"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.DAD).toStrictEqual(`dad`);
+  });
+
+  it(`should have a member "DADDY"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.DADDY).toStrictEqual(`daddy`);
+  });
+
+  it(`should have a member "DEEZER"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.DEEZER).toStrictEqual(`Deezer`);
+  });
+
+  it(`should have a member "APPLE_MUSIC"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.APPLE_MUSIC).toStrictEqual(`Apple Music`);
+  });
+
+  it(`should have a member "GOD"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.GOD).toStrictEqual(`God`);
+  });
+
+  it(`should have a member "WEB_STORM"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.WEB_STORM).toStrictEqual(`WebStorm`);
+  });
+
+  it(`should have a member "GRAND_THEFT_AUTO_V"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.GRAND_THEFT_AUTO_V).toStrictEqual(
+      `Grand Theft Auto V`
     );
   });
 
-  it(`should have a member "LISTENING"`, (): void => {
+  it(`should have a member "COUNTER_STRIKE_GLOBAL_OFFENSIVE"`, (): void => {
     expect.assertions(1);
 
-    expect(DiscordActivityTypeEnum.LISTENING).toStrictEqual(`LISTENING`);
+    expect(
+      DiscordActivityNameEnum.COUNTER_STRIKE_GLOBAL_OFFENSIVE
+    ).toStrictEqual(`Counter-Strike: Global Offensive`);
   });
 
-  it(`should have a member "PLAYING"`, (): void => {
+  it(`should have a member "MINECRAFT"`, (): void => {
     expect.assertions(1);
 
-    expect(DiscordActivityTypeEnum.PLAYING).toStrictEqual(`PLAYING`);
+    expect(DiscordActivityNameEnum.MINECRAFT).toStrictEqual(`Minecraft`);
   });
 
-  it(`should have a member "STREAMING"`, (): void => {
+  it(`should have a member "WORLD_OF_WARCRAFT"`, (): void => {
     expect.assertions(1);
 
-    expect(DiscordActivityTypeEnum.STREAMING).toStrictEqual(`STREAMING`);
+    expect(DiscordActivityNameEnum.WORLD_OF_WARCRAFT).toStrictEqual(
+      `World of Warcraft`
+    );
   });
 
-  it(`should have a member "WATCHING"`, (): void => {
+  it(`should have a member "YOU_TUBE"`, (): void => {
     expect.assertions(1);
 
-    expect(DiscordActivityTypeEnum.WATCHING).toStrictEqual(`WATCHING`);
+    expect(DiscordActivityNameEnum.YOU_TUBE).toStrictEqual(`YouTube`);
+  });
+
+  it(`should have a member "AMAZON_PRIME_VIDEO"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.AMAZON_PRIME_VIDEO).toStrictEqual(
+      `Amazon Prime Video`
+    );
+  });
+
+  it(`should have a member "OCS"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.OCS).toStrictEqual(`OCS`);
+  });
+
+  it(`should have a member "CANAL_PLUS_SERIES"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.CANAL_PLUS_SERIES).toStrictEqual(
+      `CANAL+ Series`
+    );
+  });
+
+  it(`should have a member "APPLE_TV_PLUS"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.APPLE_TV_PLUS).toStrictEqual(`Apple TV+`);
+  });
+
+  it(`should have a member "DISNEY_PLUS"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.DISNEY_PLUS).toStrictEqual(`Disney+`);
+  });
+
+  it(`should have a member "YOU_TUBE_PREMIUM"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.YOU_TUBE_PREMIUM).toStrictEqual(
+      `YouTube Premium`
+    );
+  });
+
+  it(`should have a member "FACEBOOK_WATCH"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.FACEBOOK_WATCH).toStrictEqual(
+      `Facebook Watch`
+    );
+  });
+
+  it(`should have a member "SALTO"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.SALTO).toStrictEqual(`Salto`);
+  });
+
+  it(`should have a member "PEACOCK"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.PEACOCK).toStrictEqual(`Peacock`);
+  });
+
+  it(`should have a member "HBO_MAX"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.HBO_MAX).toStrictEqual(`HBO Max`);
+  });
+
+  it(`should have a member "TWITCH"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.TWITCH).toStrictEqual(`Twitch`);
+  });
+
+  it(`should have a member "YOU"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.YOU).toStrictEqual(`you`);
+  });
+
+  it(`should have a member "LIKE_BIG_BROTHER"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.LIKE_BIG_BROTHER).toStrictEqual(
+      `like Big Brother`
+    );
+  });
+
+  it(`should have a member "SOME_MEMES"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.SOME_MEMES).toStrictEqual(`some memes`);
+  });
+
+  it(`should have a member "THE_FBI"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.THE_FBI).toStrictEqual(`the FBI`);
+  });
+
+  it(`should have a member "THE_EXCLAMATION_POINT_HELP_COMMAND"`, (): void => {
+    expect.assertions(1);
+
+    expect(
+      DiscordActivityNameEnum.THE_EXCLAMATION_POINT_HELP_COMMAND
+    ).toStrictEqual(`the !help command`);
+  });
+
+  it(`should have a member "THE_DOLLAR_HELP_COMMAND"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.THE_DOLLAR_HELP_COMMAND).toStrictEqual(
+      `the $help command`
+    );
+  });
+
+  it(`should have a member "THE_DASH_HELP_COMMAND"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.THE_DASH_HELP_COMMAND).toStrictEqual(
+      `the -help command`
+    );
+  });
+
+  it(`should have a member "SQUEEZIE_GAMING"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.SQUEEZIE_GAMING).toStrictEqual(
+      `SQUEEZIE GAMING`
+    );
+  });
+
+  it(`should have a member "A_LIVE_OF_RAMMSTEIN"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordActivityNameEnum.A_LIVE_OF_RAMMSTEIN).toStrictEqual(
+      `a live of Rammstein`
+    );
   });
 });
