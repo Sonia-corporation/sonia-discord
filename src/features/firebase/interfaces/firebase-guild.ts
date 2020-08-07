4a5a98d68a35f414aa618725eccda0c053e0676a
Geoffrey TESTELIN <geoffrey.testelin@gmail.com>
2020-08-07 19:23:22 +0200

    chore(firebase-guild): add docs for each property

--- src/features/firebase/interfaces/firebase-guild.ts
+++ src/features/firebase/interfaces/firebase-guild.ts
@@ -4,6 +4,15 @@ import { FirebaseGuildVersionEnum } from "../enums/firebase-guild-version.enum";
 import DocumentData = admin.firestore.DocumentData;
 
 export interface IFirebaseGuild extends DocumentData {
+  /**
+   * @description
+   * The Discord guild id
+   */
   id?: Snowflake | undefined;
+
+  /**
+   * @description
+   * The entity version used to perform clean update when a breaking change occur
+   */
   version?: FirebaseGuildVersionEnum | undefined;
 }
