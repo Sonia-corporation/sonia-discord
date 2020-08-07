41b29f6272d0642a7e451fc340b43666c6406717
Geoffrey TESTELIN <geoffrey.testelin@gmail.com>
2020-08-07 18:57:58 +0200

    test(firebase-guilds-store): add coverage

---
+++ src/features/firebase/stores/guilds/firebase-guilds-store.spec.ts
@@ -0,0 +1,51 @@
+import { ServiceNameEnum } from "../../../../enums/service-name.enum";
+import { CoreEventService } from "../../../core/services/core-event.service";
+import { FirebaseGuildsStore } from "./firebase-guilds-store";
+
+describe(`FirebaseGuildsStore`, (): void => {
+  let service: FirebaseGuildsStore;
+  let coreEventService: CoreEventService;
+
+  beforeEach((): void => {
+    coreEventService = CoreEventService.getInstance();
+  });
+
+  describe(`getInstance()`, (): void => {
+    it(`should create a FirebaseGuildsStore service`, (): void => {
+      expect.assertions(1);
+
+      service = FirebaseGuildsStore.getInstance();
+
+      expect(service).toStrictEqual(expect.any(FirebaseGuildsStore));
+    });
+
+    it(`should return the created FirebaseGuildsStore service`, (): void => {
+      expect.assertions(1);
+
+      const result = FirebaseGuildsStore.getInstance();
+
+      expect(result).toStrictEqual(service);
+    });
+  });
+
+  describe(`constructor()`, (): void => {
+    let coreEventServiceNotifyServiceCreatedSpy: jest.SpyInstance;
+
+    beforeEach((): void => {
+      coreEventServiceNotifyServiceCreatedSpy = jest
+        .spyOn(coreEventService, `notifyServiceCreated`)
+        .mockImplementation();
+    });
+
+    it(`should notify the FirebaseGuildsStore service creation`, (): void => {
+      expect.assertions(2);
+
+      service = new FirebaseGuildsStore();
+
+      expect(coreEventServiceNotifyServiceCreatedSpy).toHaveBeenCalledTimes(1);
+      expect(coreEventServiceNotifyServiceCreatedSpy).toHaveBeenCalledWith(
+        ServiceNameEnum.FIREBASE_GUILDS_STORE
+      );
+    });
+  });
+});
