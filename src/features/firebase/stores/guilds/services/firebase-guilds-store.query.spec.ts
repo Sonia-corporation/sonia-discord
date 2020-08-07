d0db81aa83634ace79910eb07a89e3767e9e4c3d
Geoffrey TESTELIN <geoffrey.testelin@gmail.com>
2020-08-07 19:18:36 +0200

    test(firebase-guilds-store-query): add coverage

---
+++ src/features/firebase/stores/guilds/services/firebase-guilds-store.query.spec.ts
@@ -0,0 +1,54 @@
+import { ServiceNameEnum } from "../../../../../enums/service-name.enum";
+import { CoreEventService } from "../../../../core/services/core-event.service";
+import { FirebaseGuildsStore } from "../firebase-guilds-store";
+import { FirebaseGuildsStoreQuery } from "./firebase-guilds-store.query";
+
+describe(`FirebaseGuildsStoreQuery`, (): void => {
+  let service: FirebaseGuildsStoreQuery;
+  let coreEventService: CoreEventService;
+  let firebaseGuildsStore: FirebaseGuildsStore;
+
+  beforeEach((): void => {
+    coreEventService = CoreEventService.getInstance();
+    firebaseGuildsStore = FirebaseGuildsStore.getInstance();
+  });
+
+  describe(`getInstance()`, (): void => {
+    it(`should create a FirebaseGuildsStore query`, (): void => {
+      expect.assertions(1);
+
+      service = FirebaseGuildsStoreQuery.getInstance();
+
+      expect(service).toStrictEqual(expect.any(FirebaseGuildsStoreQuery));
+    });
+
+    it(`should return the created FirebaseGuildsStore query`, (): void => {
+      expect.assertions(1);
+
+      const result = FirebaseGuildsStoreQuery.getInstance();
+
+      expect(result).toStrictEqual(service);
+    });
+  });
+
+  describe(`constructor()`, (): void => {
+    let coreEventServiceNotifyServiceCreatedSpy: jest.SpyInstance;
+
+    beforeEach((): void => {
+      coreEventServiceNotifyServiceCreatedSpy = jest
+        .spyOn(coreEventService, `notifyServiceCreated`)
+        .mockImplementation();
+    });
+
+    it(`should notify the FirebaseGuildsStore query creation`, (): void => {
+      expect.assertions(2);
+
+      service = new FirebaseGuildsStoreQuery(firebaseGuildsStore);
+
+      expect(coreEventServiceNotifyServiceCreatedSpy).toHaveBeenCalledTimes(1);
+      expect(coreEventServiceNotifyServiceCreatedSpy).toHaveBeenCalledWith(
+        ServiceNameEnum.FIREBASE_GUILDS_STORE_QUERY
+      );
+    });
+  });
+});
