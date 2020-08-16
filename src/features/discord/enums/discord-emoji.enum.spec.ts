7bd3fb2105e6b6c7697875208d00944b59337497
Geoffrey TESTELIN <geoffrey.testelin@gmail.com>
2020-08-16 17:17:49 +0200

    chore(emoji): add face with raised eyebrow and gift heart emojis

--- src/features/discord/enums/discord-emoji.enum.spec.ts
+++ src/features/discord/enums/discord-emoji.enum.spec.ts
@@ -1,12 +1,26 @@
 import { DiscordEmojiEnum } from "./discord-emoji.enum";
 
 describe(`DiscordEmojiEnum`, (): void => {
+  it(`should have a member "FACE_WITH_RAISED_EYEBROW"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordEmojiEnum.FACE_WITH_RAISED_EYEBROW).toStrictEqual(
+      `:face_with_raised_eyebrow:`
+    );
+  });
+
   it(`should have a member "FACE_WITH_SYMBOLS_OVER_MOUSE"`, (): void => {
     expect.assertions(1);
 
     expect(DiscordEmojiEnum.FACE_WITH_SYMBOLS_OVER_MOUSE).toStrictEqual(
       `:face_with_symbols_over_mouth:`
     );
+  });
+
+  it(`should have a member "GIFT_HEART"`, (): void => {
+    expect.assertions(1);
+
+    expect(DiscordEmojiEnum.GIFT_HEART).toStrictEqual(`:gift_heart:`);
   });
 
   it(`should have a member "PRAY"`, (): void => {
--- src/features/discord/enums/discord-emoji.enum.ts
+++ src/features/discord/enums/discord-emoji.enum.ts
@@ -1,5 +1,7 @@
 export enum DiscordEmojiEnum {
+  FACE_WITH_RAISED_EYEBROW = `:face_with_raised_eyebrow:`,
   FACE_WITH_SYMBOLS_OVER_MOUSE = `:face_with_symbols_over_mouth:`,
+  GIFT_HEART = `:gift_heart:`,
   PRAY = `:pray:`,
   THINKING = `:thinking:`,
   WARNING = `:warning:`,
