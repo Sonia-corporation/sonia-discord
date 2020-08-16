edc817111ffd3a7054115f240fe1cf7dddc67017
Geoffrey TESTELIN <geoffrey.testelin@gmail.com>
2020-08-16 12:32:37 +0200

    feat(feature): respond an embed error message when the feature content is empty

---
+++ src/features/discord/messages/services/command/feature/discord-message-command-feature-error.service.spec.ts
@@ -0,0 +1,345 @@
+import {
+  EmbedFieldData,
+  MessageEmbedAuthor,
+  MessageEmbedFooter,
+  MessageEmbedThumbnail,
+} from "discord.js";
+import moment from "moment-timezone";
+import { createMock } from "ts-auto-mock";
+import { ColorEnum } from "../../../../../../enums/color.enum";
+import { IconEnum } from "../../../../../../enums/icon.enum";
+import { ServiceNameEnum } from "../../../../../../enums/service-name.enum";
+import { CoreEventService } from "../../../../../core/services/core-event.service";
+import { GithubConfigService } from "../../../../../github/services/config/github-config.service";
+import { DiscordGuildConfigService } from "../../../../guilds/services/config/discord-guild-config.service";
+import { DiscordSoniaService } from "../../../../users/services/discord-sonia.service";
+import { DiscordMessageConfigService } from "../../config/discord-message-config.service";
+import { DiscordMessageCommandCliErrorService } from "../discord-message-command-cli-error.service";
+import { DiscordMessageCommandFeatureErrorService } from "./discord-message-command-feature-error.service";
+
+describe(`DiscordMessageCommandFeatureErrorService`, (): void => {
+  let service: DiscordMessageCommandFeatureErrorService;
+  let coreEventService: CoreEventService;
+  let discordSoniaService: DiscordSoniaService;
+  let discordMessageConfigService: DiscordMessageConfigService;
+  let discordMessageCommandCliErrorService: DiscordMessageCommandCliErrorService;
+  let githubConfigService: GithubConfigService;
+  let discordGuildConfigService: DiscordGuildConfigService;
+
+  beforeEach((): void => {
+    coreEventService = CoreEventService.getInstance();
+    discordSoniaService = DiscordSoniaService.getInstance();
+    discordMessageConfigService = DiscordMessageConfigService.getInstance();
+    discordMessageCommandCliErrorService = DiscordMessageCommandCliErrorService.getInstance();
+    githubConfigService = GithubConfigService.getInstance();
+    discordGuildConfigService = DiscordGuildConfigService.getInstance();
+  });
+
+  describe(`getInstance()`, (): void => {
+    it(`should create a DiscordMessageCommandFeatureError service`, (): void => {
+      expect.assertions(1);
+
+      service = DiscordMessageCommandFeatureErrorService.getInstance();
+
+      expect(service).toStrictEqual(
+        expect.any(DiscordMessageCommandFeatureErrorService)
+      );
+    });
+
+    it(`should return the created DiscordMessageCommandFeatureError service`, (): void => {
+      expect.assertions(1);
+
+      const result = DiscordMessageCommandFeatureErrorService.getInstance();
+
+      expect(result).toStrictEqual(service);
+    });
+  });
+
+  describe(`constructor()`, (): void => {
+    let coreEventServiceNotifyServiceCreatedSpy: jest.SpyInstance;
+
+    beforeEach((): void => {
+      coreEventServiceNotifyServiceCreatedSpy = jest
+        .spyOn(coreEventService, `notifyServiceCreated`)
+        .mockImplementation();
+    });
+
+    it(`should notify the DiscordMessageCommandFeatureError service creation`, (): void => {
+      expect.assertions(2);
+
+      service = new DiscordMessageCommandFeatureErrorService();
+
+      expect(coreEventServiceNotifyServiceCreatedSpy).toHaveBeenCalledTimes(1);
+      expect(coreEventServiceNotifyServiceCreatedSpy).toHaveBeenCalledWith(
+        ServiceNameEnum.DISCORD_MESSAGE_COMMAND_FEATURE_ERROR_SERVICE
+      );
+    });
+  });
+
+  describe(`getEmptyContentErrorMessageResponse()`, (): void => {
+    let discordMessageCommandCliErrorServiceGetCliErrorMessageResponseSpy: jest.SpyInstance;
+    let discordSoniaServiceGetCorporationMessageEmbedAuthorSpy: jest.SpyInstance;
+    let discordMessageConfigServiceGetMessageCommandCliErrorImageColorSpy: jest.SpyInstance;
+    let discordSoniaServiceGetImageUrlSpy: jest.SpyInstance;
+    let discordMessageConfigServiceGetMessageCommandCliErrorImageUrlSpy: jest.SpyInstance;
+    let githubConfigServiceGetBugReportUrlSpy: jest.SpyInstance;
+    let discordGuildConfigServiceGetSoniaPermanentGuildInviteUrlSpy: jest.SpyInstance;
+
+    beforeEach((): void => {
+      service = new DiscordMessageCommandFeatureErrorService();
+
+      discordMessageCommandCliErrorServiceGetCliErrorMessageResponseSpy = jest.spyOn(
+        discordMessageCommandCliErrorService,
+        `getCliErrorMessageResponse`
+      );
+      discordSoniaServiceGetCorporationMessageEmbedAuthorSpy = jest.spyOn(
+        discordSoniaService,
+        `getCorporationMessageEmbedAuthor`
+      );
+      discordMessageConfigServiceGetMessageCommandCliErrorImageColorSpy = jest.spyOn(
+        discordMessageConfigService,
+        `getMessageCommandCliErrorImageColor`
+      );
+      discordSoniaServiceGetImageUrlSpy = jest.spyOn(
+        discordSoniaService,
+        `getImageUrl`
+      );
+      discordMessageConfigServiceGetMessageCommandCliErrorImageUrlSpy = jest.spyOn(
+        discordMessageConfigService,
+        `getMessageCommandCliErrorImageUrl`
+      );
+      githubConfigServiceGetBugReportUrlSpy = jest.spyOn(
+        githubConfigService,
+        `getBugReportUrl`
+      );
+      discordGuildConfigServiceGetSoniaPermanentGuildInviteUrlSpy = jest.spyOn(
+        discordGuildConfigService,
+        `getSoniaPermanentGuildInviteUrl`
+      );
+    });
+
+    it(`should get the CLI error message response`, async (): Promise<void> => {
+      expect.assertions(2);
+
+      await service.getEmptyContentErrorMessageResponse();
+
+      expect(
+        discordMessageCommandCliErrorServiceGetCliErrorMessageResponseSpy
+      ).toHaveBeenCalledTimes(1);
+      expect(
+        discordMessageCommandCliErrorServiceGetCliErrorMessageResponseSpy
+      ).toHaveBeenCalledWith();
+    });
+
+    it(`should return a Discord message response embed with an author`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+      const messageEmbedAuthor: MessageEmbedAuthor = createMock<
+        MessageEmbedAuthor
+      >();
+      discordSoniaServiceGetCorporationMessageEmbedAuthorSpy.mockReturnValue(
+        messageEmbedAuthor
+      );
+
+      const result = await service.getEmptyContentErrorMessageResponse();
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      expect(result.options.embed.author).toStrictEqual(messageEmbedAuthor);
+    });
+
+    it(`should return a Discord message response embed with a color`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+      discordMessageConfigServiceGetMessageCommandCliErrorImageColorSpy.mockReturnValue(
+        ColorEnum.CANDY
+      );
+
+      const result = await service.getEmptyContentErrorMessageResponse();
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      expect(result.options.embed.color).toStrictEqual(ColorEnum.CANDY);
+    });
+
+    it(`should return a Discord message response embed with 2 fields`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+
+      const result = await service.getEmptyContentErrorMessageResponse();
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      expect(result.options.embed.fields).toHaveLength(2);
+    });
+
+    it(`should return a Discord message response embed with a field explaining the error`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+
+      const result = await service.getEmptyContentErrorMessageResponse();
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      expect(result.options.embed.fields[0]).toStrictEqual({
+        name: `Empty content`,
+        value: `The content of the message is empty. I can not process the command nevertheless this should never happen. Do not be so selfish and share this information!`,
+      } as EmbedFieldData);
+    });
+
+    it(`should return a Discord message response embed with a field to report the error`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+      githubConfigServiceGetBugReportUrlSpy.mockReturnValue(
+        `dummy-bug-report-url`
+      );
+      discordGuildConfigServiceGetSoniaPermanentGuildInviteUrlSpy.mockReturnValue(
+        `dummy-sonia-permanent-guild-invite-url`
+      );
+
+      const result = await service.getEmptyContentErrorMessageResponse();
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      expect(result.options.embed.fields[1]).toStrictEqual({
+        name: `Help me to help you`,
+        value: `You can create a [bug report](dummy-bug-report-url) or reach my creators on [discord](dummy-sonia-permanent-guild-invite-url).`,
+      } as EmbedFieldData);
+    });
+
+    it(`should return a Discord message response embed with a footer containing an icon and a text`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+      discordSoniaServiceGetImageUrlSpy.mockReturnValue(`dummy-image-url`);
+
+      const result = await service.getEmptyContentErrorMessageResponse();
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      expect(result.options.embed.footer).toStrictEqual({
+        iconURL: `dummy-image-url`,
+        text: `Invalid feature command`,
+      } as MessageEmbedFooter);
+    });
+
+    describe(`when the Sonia image url is null`, (): void => {
+      beforeEach((): void => {
+        discordSoniaServiceGetImageUrlSpy.mockReturnValue(null);
+      });
+
+      it(`should return a Discord message response embed with a footer but without an icon`, async (): Promise<
+        void
+      > => {
+        expect.assertions(1);
+
+        const result = await service.getEmptyContentErrorMessageResponse();
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        expect(result.options.embed.footer).toStrictEqual({
+          iconURL: undefined,
+          text: `Invalid feature command`,
+        } as MessageEmbedFooter);
+      });
+    });
+
+    describe(`when the Sonia image url is "image-url"`, (): void => {
+      beforeEach((): void => {
+        discordSoniaServiceGetImageUrlSpy.mockReturnValue(`image-url`);
+      });
+
+      it(`should return a Discord message response embed with a footer containing an icon and a text`, async (): Promise<
+        void
+      > => {
+        expect.assertions(1);
+
+        const result = await service.getEmptyContentErrorMessageResponse();
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        expect(result.options.embed.footer).toStrictEqual({
+          iconURL: `image-url`,
+          text: `Invalid feature command`,
+        } as MessageEmbedFooter);
+      });
+    });
+
+    it(`should return a Discord message response embed with a thumbnail`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+      discordMessageConfigServiceGetMessageCommandCliErrorImageUrlSpy.mockReturnValue(
+        IconEnum.ARTIFICIAL_INTELLIGENCE
+      );
+
+      const result = await service.getEmptyContentErrorMessageResponse();
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      expect(result.options.embed.thumbnail).toStrictEqual({
+        url: IconEnum.ARTIFICIAL_INTELLIGENCE,
+      } as MessageEmbedThumbnail);
+    });
+
+    it(`should return a Discord message response embed with a timestamp`, async (): Promise<
+      void
+    > => {
+      expect.assertions(2);
+
+      const result = await service.getEmptyContentErrorMessageResponse();
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      expect(moment(result.options.embed.timestamp).isValid()).toStrictEqual(
+        true
+      );
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      expect(moment(result.options.embed.timestamp).fromNow()).toStrictEqual(
+        `a few seconds ago`
+      );
+    });
+
+    it(`should return a Discord message response embed with a title`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+
+      const result = await service.getEmptyContentErrorMessageResponse();
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      expect(result.options.embed.title).toStrictEqual(
+        `I can not handle your request :worried:`
+      );
+    });
+
+    it(`should return a Discord message response splitted`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+
+      const result = await service.getEmptyContentErrorMessageResponse();
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      expect(result.options.split).toStrictEqual(true);
+    });
+
+    it(`should return a Discord message response without a response text`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+
+      const result = await service.getEmptyContentErrorMessageResponse();
+
+      expect(result.response).toStrictEqual(``);
+    });
+  });
+});
---
+++ src/features/discord/messages/services/command/feature/discord-message-command-feature-error.service.ts
@@ -0,0 +1,99 @@
+import {
+  EmbedFieldData,
+  MessageEmbedFooter,
+  MessageEmbedOptions,
+} from "discord.js";
+import _ from "lodash";
+import { AbstractService } from "../../../../../../classes/abstract.service";
+import { ServiceNameEnum } from "../../../../../../enums/service-name.enum";
+import { GithubConfigService } from "../../../../../github/services/config/github-config.service";
+import { DiscordEmojiEnum } from "../../../../enums/discord-emoji.enum";
+import { DiscordGuildConfigService } from "../../../../guilds/services/config/discord-guild-config.service";
+import { DiscordSoniaService } from "../../../../users/services/discord-sonia.service";
+import { IDiscordMessageResponse } from "../../../interfaces/discord-message-response";
+import { DiscordMessageCommandCliErrorService } from "../discord-message-command-cli-error.service";
+
+export class DiscordMessageCommandFeatureErrorService extends AbstractService {
+  private static _instance: DiscordMessageCommandFeatureErrorService;
+
+  public static getInstance(): DiscordMessageCommandFeatureErrorService {
+    if (_.isNil(DiscordMessageCommandFeatureErrorService._instance)) {
+      DiscordMessageCommandFeatureErrorService._instance = new DiscordMessageCommandFeatureErrorService();
+    }
+
+    return DiscordMessageCommandFeatureErrorService._instance;
+  }
+
+  public constructor() {
+    super(ServiceNameEnum.DISCORD_MESSAGE_COMMAND_FEATURE_ERROR_SERVICE);
+  }
+
+  public getEmptyContentErrorMessageResponse(): Promise<
+    IDiscordMessageResponse
+  > {
+    return DiscordMessageCommandCliErrorService.getInstance()
+      .getCliErrorMessageResponse()
+      .then(
+        (
+          cliErrorMessageResponse: Readonly<IDiscordMessageResponse>
+        ): Promise<IDiscordMessageResponse> => {
+          return Promise.resolve(
+            _.merge(cliErrorMessageResponse, {
+              options: {
+                embed: this._getEmptyContentErrorMessageEmbed(),
+                split: true,
+              },
+              response: ``,
+            } as IDiscordMessageResponse)
+          );
+        }
+      );
+  }
+
+  private _getEmptyContentErrorMessageEmbed(): MessageEmbedOptions {
+    return {
+      fields: this._getEmptyContentErrorMessageEmbedFields(),
+      footer: this._getEmptyContentErrorMessageEmbedFooter(),
+      title: this._getEmptyContentErrorMessageEmbedTitle(),
+    };
+  }
+
+  private _getEmptyContentErrorMessageEmbedFields(): EmbedFieldData[] {
+    return [
+      this._getEmptyContentErrorMessageEmbedFieldError(),
+      this._getEmptyContentErrorMessageEmbedFieldErrorReport(),
+    ];
+  }
+
+  private _getEmptyContentErrorMessageEmbedFieldError(): EmbedFieldData {
+    return {
+      name: `Empty content`,
+      value: `The content of the message is empty. I can not process the command nevertheless this should never happen. Do not be so selfish and share this information!`,
+    };
+  }
+
+  private _getEmptyContentErrorMessageEmbedFieldErrorReport(): EmbedFieldData {
+    const githubBugReportUrl: string = GithubConfigService.getInstance().getBugReportUrl();
+    const discordSoniaPermanentGuildInviteUrl: string = DiscordGuildConfigService.getInstance().getSoniaPermanentGuildInviteUrl();
+
+    return {
+      name: `Help me to help you`,
+      value: `You can create a [bug report](${githubBugReportUrl}) or reach my creators on [discord](${discordSoniaPermanentGuildInviteUrl}).`,
+    };
+  }
+
+  private _getEmptyContentErrorMessageEmbedFooter(): MessageEmbedFooter {
+    const soniaImageUrl:
+      | string
+      | null = DiscordSoniaService.getInstance().getImageUrl();
+
+    return {
+      iconURL: soniaImageUrl || undefined,
+      text: `Invalid feature command`,
+    };
+  }
+
+  private _getEmptyContentErrorMessageEmbedTitle(): string {
+    return `I can not handle your request ${DiscordEmojiEnum.WORRIED}`;
+  }
+}
