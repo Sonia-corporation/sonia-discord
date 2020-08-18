42958fc9479b24e3de41bc5018c55ad7a917f7a6
Geoffrey TESTELIN <geoffrey.testelin@gmail.com>
2020-08-18 14:32:39 +0200

    chore(mention): create a new function to wrap an id as a mention

---
+++ src/features/discord/mentions/functions/wrap-user-id-into-mention.spec.ts
@@ -0,0 +1,18 @@
+import { Snowflake } from "discord.js";
+import { wrapUserIdIntoMention } from "./wrap-user-id-into-mention";
+
+describe(`wrapUserIdIntoMention()`, (): void => {
+  let userId: Snowflake;
+
+  beforeEach((): void => {
+    userId = `dummy-user-id`;
+  });
+
+  it(`should return the given user id wrapped into a Discord mention`, (): void => {
+    expect.assertions(1);
+
+    const result = wrapUserIdIntoMention(userId);
+
+    expect(result).toStrictEqual(`<@!dummy-user-id>`);
+  });
+});
