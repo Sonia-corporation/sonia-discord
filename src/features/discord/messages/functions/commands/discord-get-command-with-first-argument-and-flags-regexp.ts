c30f44972f1cb8aa0c98497d7377174ffd10051f
Geoffrey TESTELIN <geoffrey.testelin@gmail.com>
2020-08-21 21:25:17 +0200

    chore(discordGetCommandWithFirstArgumentAndFlagsRegexp): add a description
    the description explains the flags part of the regexp

--- src/features/discord/messages/functions/commands/discord-get-command-with-first-argument-and-flags-regexp.ts
+++ src/features/discord/messages/functions/commands/discord-get-command-with-first-argument-and-flags-regexp.ts
@@ -2,6 +2,12 @@ import xregexp from "xregexp";
 import { IDiscordGetCommandWithFirstArgumentAndFlagsRegexpData } from "../../interfaces/commands/discord-get-command-with-first-argument-and-flags-regexp-data";
 
 /**
+ * @description
+ * For the flag part it allows:
+ * - Shortcut flag (-flag)
+ * - Normal flag (--flag)
+ * - Value flag (--flag=xxx)
+ *
  * @param {Readonly<IDiscordGetCommandWithFirstArgumentAndFlagsRegexpData>} data The data used as a command
  *
  * @return {RegExp} A RegExp matching a prefix with a command, one argument and optional flags
