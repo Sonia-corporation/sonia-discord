f1b8071d238d8f9ddc4563e8cd49de3fcbf2a171
Geoffrey TESTELIN <geoffrey.testelin@gmail.com>
2020-08-26 18:13:11 +0200

    build(test): clear mocks
    undo the previous commit
    this is useful to avoid errors after a second run

--- jest.config.js
+++ jest.config.js
@@ -17,7 +17,7 @@ module.exports = {
   cacheDirectory: `./.cache-jest`,
 
   // Automatically clear mock calls and instances between every test
-  clearMocks: false,
+  clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: false,
