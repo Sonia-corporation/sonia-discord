c09fe0eeaacfae91aefa9c6b4940f3bfb3ff3b90
Geoffrey TESTELIN <geoffrey.testelin@gmail.com>
2020-08-27 22:54:29 +0200

    docs(getRandomValueFromEnum): enhance the jsdoc

--- src/functions/randoms/get-random-value-from-enum.ts
+++ src/functions/randoms/get-random-value-from-enum.ts
@@ -1,7 +1,14 @@
 import _ from "lodash";
 
-// Keep in mind that string enum members do not get a reverse mapping generated at all
-// Using other types could lead to having keys as values
+/**
+ * @description
+ * Keep in mind that string enum members do not get a reverse mapping generated at all
+ * Using other types could lead to having keys as values
+ *
+ * @param {Record<string, TValue>} enumeration The enumeration to pick from
+ *
+ * @return {TValue | undefined} A random value from the enum
+ */
 export function getRandomValueFromEnum<TValue>(
   enumeration: Record<string, TValue>
 ): TValue | undefined {
