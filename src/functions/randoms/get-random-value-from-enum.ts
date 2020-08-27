d73ec7f0b915835b2fc3a3b1a03cf608e3ce6e82
Geoffrey TESTELIN <geoffrey.testelin@gmail.com>
2020-08-27 22:52:59 +0200

    refactor(getRandomValueFromEnum): change the types and use lodash

--- src/functions/randoms/get-random-value-from-enum.ts
+++ src/functions/randoms/get-random-value-from-enum.ts
@@ -3,7 +3,7 @@ import _ from "lodash";
 // Keep in mind that string enum members do not get a reverse mapping generated at all
 // Using other types could lead to having keys as values
 export function getRandomValueFromEnum<TValue>(
-  enumeration: { [s: string]: TValue } | ArrayLike<TValue>
+  enumeration: Record<string, TValue>
 ): TValue | undefined {
-  return _.sample(Object.values(enumeration)) as TValue;
+  return _.sample(_.values(enumeration)) as TValue;
 }
