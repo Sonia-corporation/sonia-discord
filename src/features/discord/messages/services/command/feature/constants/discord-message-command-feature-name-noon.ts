7d9f91791f841e1c8d01f5378c0ae4d4fa16d967
Geoffrey TESTELIN <geoffrey.testelin@gmail.com>
2020-08-28 18:09:03 +0200

    refactor(constants): split the command feature name for noon to a standalone constant

---
+++ src/features/discord/messages/services/command/feature/constants/discord-message-command-feature-name-noon.ts
@@ -0,0 +1,10 @@
+import { DiscordCommandFirstArgument } from "../../../../classes/commands/arguments/discord-command-first-argument";
+import { DiscordMessageCommandFeatureNameEnum } from "../enums/discord-message-command-feature-name.enum";
+
+export const DISCORD_MESSAGE_COMMAND_FEATURE_NAME_NOON: DiscordCommandFirstArgument<DiscordMessageCommandFeatureNameEnum> = new DiscordCommandFirstArgument(
+  {
+    description: `Configure the noon message sent at 12AM.`,
+    name: DiscordMessageCommandFeatureNameEnum.NOON,
+    shortcuts: [DiscordMessageCommandFeatureNameEnum.N],
+  }
+);
--- src/features/discord/messages/services/command/feature/constants/discord-message-command-feature-names.ts
+++ src/features/discord/messages/services/command/feature/constants/discord-message-command-feature-names.ts
@@ -1,13 +1,7 @@
-import { DiscordCommandFirstArgument } from "../../../../classes/commands/arguments/discord-command-first-argument";
 import { DiscordCommandFirstArguments } from "../../../../classes/commands/arguments/discord-command-first-arguments";
 import { DiscordMessageCommandFeatureNameEnum } from "../enums/discord-message-command-feature-name.enum";
+import { DISCORD_MESSAGE_COMMAND_FEATURE_NAME_NOON } from "./discord-message-command-feature-name-noon";
 
 export const DISCORD_MESSAGE_COMMAND_FEATURE_NAMES: DiscordCommandFirstArguments<DiscordMessageCommandFeatureNameEnum> = new DiscordCommandFirstArguments(
-  [
-    new DiscordCommandFirstArgument({
-      description: `Configure the noon message sent at 12AM.`,
-      name: DiscordMessageCommandFeatureNameEnum.NOON,
-      shortcuts: [DiscordMessageCommandFeatureNameEnum.N],
-    }),
-  ]
+  [DISCORD_MESSAGE_COMMAND_FEATURE_NAME_NOON]
 );
