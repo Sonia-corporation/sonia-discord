645397b76b7105263c8b28d8a7cd6cdd011fbcaa
Geoffrey TESTELIN <geoffrey.testelin@gmail.com>
2020-09-04 00:31:10 +0200

    chore(flag): add new function to get the flag value

---
+++ src/features/discord/messages/functions/commands/flags/discord-command-get-flag-value.spec.ts
@@ -0,0 +1,104 @@
+import { IDiscordMessageFlag } from "../../../types/commands/flags/discord-message-flag";
+import { discordCommandGetFlagValue } from "./discord-command-get-flag-value";
+
+describe(`discordCommandGetFlagValue()`, (): void => {
+  let messageFlag: IDiscordMessageFlag;
+
+  describe(`when the given flag is empty`, (): void => {
+    beforeEach((): void => {
+      messageFlag = ``;
+    });
+
+    it(`should return null`, (): void => {
+      expect.assertions(1);
+
+      const result = discordCommandGetFlagValue(messageFlag);
+
+      expect(result).toBeNull();
+    });
+  });
+
+  describe(`when the given flag is a shortcut flag`, (): void => {
+    beforeEach((): void => {
+      messageFlag = `-flag`;
+    });
+
+    it(`should return null`, (): void => {
+      expect.assertions(1);
+
+      const result = discordCommandGetFlagValue(messageFlag);
+
+      expect(result).toBeNull();
+    });
+  });
+
+  describe(`when the given flag is a shortcut flag with an empty value`, (): void => {
+    beforeEach((): void => {
+      messageFlag = `-flag=`;
+    });
+
+    it(`should return null`, (): void => {
+      expect.assertions(1);
+
+      const result = discordCommandGetFlagValue(messageFlag);
+
+      expect(result).toBeNull();
+    });
+  });
+
+  describe(`when the given flag is a shortcut flag with a value`, (): void => {
+    beforeEach((): void => {
+      messageFlag = `-flag=dummy`;
+    });
+
+    it(`should return null`, (): void => {
+      expect.assertions(1);
+
+      const result = discordCommandGetFlagValue(messageFlag);
+
+      expect(result).toBeNull();
+    });
+  });
+
+  describe(`when the given flag is a flag`, (): void => {
+    beforeEach((): void => {
+      messageFlag = `--flag`;
+    });
+
+    it(`should return null`, (): void => {
+      expect.assertions(1);
+
+      const result = discordCommandGetFlagValue(messageFlag);
+
+      expect(result).toBeNull();
+    });
+  });
+
+  describe(`when the given flag is a flag with an empty value`, (): void => {
+    beforeEach((): void => {
+      messageFlag = `--flag=`;
+    });
+
+    it(`should return null`, (): void => {
+      expect.assertions(1);
+
+      const result = discordCommandGetFlagValue(messageFlag);
+
+      expect(result).toBeNull();
+    });
+  });
+
+  describe(`when the given flag is a flag with a value`, (): void => {
+    beforeEach((): void => {
+      messageFlag = `--flag=dummy`;
+    });
+
+    it(`should return "dummy"`, (): void => {
+      expect.assertions(1);
+
+      const result = discordCommandGetFlagValue(messageFlag);
+
+      expect(result).toStrictEqual(`dummy`);
+    });
+  });
+});
---
+++ src/features/discord/messages/functions/commands/flags/discord-command-get-flag-value.ts
@@ -0,0 +1,40 @@
+import _ from "lodash";
+import { IDiscordMessageFlag } from "../../../types/commands/flags/discord-message-flag";
+import { discordCommandRemoveFlagPrefix } from "./discord-command-remove-flag-prefix";
+
+const SPLITTED_FLAG_LENGTH = 2;
+
+/**
+ * @description
+ * Extract the flag value from a message flag
+ *
+ * @example
+ * discordCommandGetFlagValue('')          => null
+ * discordCommandGetFlagValue('-f')        => null
+ * discordCommandGetFlagValue('-f=')       => null
+ * discordCommandGetFlagValue('-f=dummy')  => null
+ * discordCommandGetFlagValue('--f')       => null
+ * discordCommandGetFlagValue('--f=')      => null
+ * discordCommandGetFlagValue('--f=dummy') => dummy
+ *
+ * @param {Readonly<IDiscordMessageFlag>} messageFlag A flag as a message
+ *
+ * @return {string | null} a string when the flag value exists
+ */
+export function discordCommandGetFlagValue(
+  messageFlag: Readonly<IDiscordMessageFlag>
+): string | null {
+  const splittedFlag: string[] = _.split(
+    discordCommandRemoveFlagPrefix(messageFlag),
+    `=`
+  );
+  const splittedFlagSize: number = _.size(splittedFlag);
+
+  if (_.lt(splittedFlagSize, SPLITTED_FLAG_LENGTH)) {
+    return null;
+  }
+
+  const flagValue: string | undefined = _.last(splittedFlag);
+
+  return _.isNil(flagValue) || _.isEmpty(flagValue) ? null : flagValue;
+}
