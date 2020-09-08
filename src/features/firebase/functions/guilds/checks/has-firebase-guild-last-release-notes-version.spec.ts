166e953dd3dd30086b0b4457db6484394ed8649b
Geoffrey TESTELIN <geoffrey.testelin@gmail.com>
2020-09-08 20:12:29 +0200

    chore(firebase): add new function to check if the guild has a last release notes version

---
+++ src/features/firebase/functions/guilds/checks/has-firebase-guild-last-release-notes-version.spec.ts
@@ -0,0 +1,58 @@
+import { createMock } from "ts-auto-mock";
+import { IFirebaseGuildV1 } from "../../../interfaces/guilds/firebase-guild-v1";
+import { IFirebaseGuildV2 } from "../../../interfaces/guilds/firebase-guild-v2";
+import { IFirebaseGuildV3 } from "../../../interfaces/guilds/firebase-guild-v3";
+import { IFirebaseGuild } from "../../../types/guilds/firebase-guild";
+import { hasFirebaseGuildLastReleaseNotesVersion } from "./has-firebase-guild-last-release-notes-version";
+
+describe(`hasFirebaseGuildLastReleaseNotesVersion()`, (): void => {
+  let firebaseGuild: IFirebaseGuild;
+
+  describe(`when the given Firebase guild is a v1`, (): void => {
+    beforeEach((): void => {
+      firebaseGuild = createMock<IFirebaseGuildV1>();
+    });
+
+    it(`should return false`, (): void => {
+      expect.assertions(1);
+
+      const hasProperty = hasFirebaseGuildLastReleaseNotesVersion(
+        firebaseGuild
+      );
+
+      expect(hasProperty).toStrictEqual(false);
+    });
+  });
+
+  describe(`when the given Firebase guild is a v2`, (): void => {
+    beforeEach((): void => {
+      firebaseGuild = createMock<IFirebaseGuildV2>();
+    });
+
+    it(`should return true`, (): void => {
+      expect.assertions(1);
+
+      const hasProperty = hasFirebaseGuildLastReleaseNotesVersion(
+        firebaseGuild
+      );
+
+      expect(hasProperty).toStrictEqual(true);
+    });
+  });
+
+  describe(`when the given Firebase guild is a v3`, (): void => {
+    beforeEach((): void => {
+      firebaseGuild = createMock<IFirebaseGuildV3>();
+    });
+
+    it(`should return true`, (): void => {
+      expect.assertions(1);
+
+      const hasProperty = hasFirebaseGuildLastReleaseNotesVersion(
+        firebaseGuild
+      );
+
+      expect(hasProperty).toStrictEqual(true);
+    });
+  });
+});
---
+++ src/features/firebase/functions/guilds/checks/has-firebase-guild-last-release-notes-version.ts
@@ -0,0 +1,24 @@
+import _ from "lodash";
+import { FirebaseGuildVersionEnum } from "../../../enums/guilds/firebase-guild-version.enum";
+import { IFirebaseGuildV2 } from "../../../interfaces/guilds/firebase-guild-v2";
+import { IFirebaseGuildV3 } from "../../../interfaces/guilds/firebase-guild-v3";
+import { IFirebaseGuild } from "../../../types/guilds/firebase-guild";
+
+/**
+ * @description
+ * Check if the given Firebase guild contains a [last release notes version]{@link IFirebaseGuildV2#lastReleaseNotesVersion}
+ *
+ * @see [sonia-link-001]{@link https://github.com/Sonia-corporation/il-est-midi-discord/blob/master/CONTRIBUTING.md#sonia-link-001}
+ *
+ * @param {Readonly<IFirebaseGuild>} firebaseGuild The Firebase guild
+ *
+ * @return {boolean} true when the given guild is at least [v2]{@link FirebaseGuildVersionEnum.V2}
+ */
+export function hasFirebaseGuildLastReleaseNotesVersion(
+  firebaseGuild: Readonly<IFirebaseGuild>
+): firebaseGuild is IFirebaseGuildV2 | IFirebaseGuildV3 {
+  return _.includes(
+    [FirebaseGuildVersionEnum.V2, FirebaseGuildVersionEnum.V3],
+    firebaseGuild.version
+  );
+}
