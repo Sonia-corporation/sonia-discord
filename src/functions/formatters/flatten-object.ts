65f18e0015285e2306014fc181142c3844b04cf5
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-10-01 17:24:30 +0200

    chore(object): add new function to flatten an object

---
+++ src/functions/formatters/flatten-object.ts
@@ -0,0 +1,25 @@
+import _ from "lodash";
+import { IObject } from "../../types/object";
+
+function flatten(
+  object: Readonly<IObject> | Readonly<string>[] | Readonly<unknown>,
+  path: Readonly<string>[] = []
+): IObject {
+  if (!_.isObject(object)) {
+    return { [path.join(`.`)]: object };
+  }
+
+  return _.reduce(
+    object,
+    (
+      cum: Readonly<IObject>,
+      next: Readonly<unknown>,
+      key: Readonly<string>
+    ): IObject => _.merge(cum, flatten(next, [...path, key])),
+    {}
+  );
+}
+
+export function flattenObject(object: Readonly<IObject>): IObject {
+  return flatten(object);
+}
