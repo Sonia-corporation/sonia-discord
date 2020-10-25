53e767babb6c6d15b45ac90df3c801011426de2a
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-10-25 15:47:20 +0100

    feat(noon-command): add the disabled flag (--disabled or -d)

--- src/features/discord/messages/services/command/feature/features/noon/constants/discord-message-command-feature-noon-flags.spec.ts
+++ src/features/discord/messages/services/command/feature/features/noon/constants/discord-message-command-feature-noon-flags.spec.ts
@@ -2,6 +2,7 @@ import { TimezoneEnum } from "../../../../../../../../time/enums/timezone.enum";
 import { DiscordCommandBooleanFlag } from "../../../../../../classes/commands/flags/discord-command-boolean-flag";
 import { DiscordCommandFlags } from "../../../../../../classes/commands/flags/discord-command-flags";
 import { DISCORD_MESSAGE_COMMAND_FEATURE_NAME_NOON } from "../../../constants/discord-message-command-feature-name-noon";
+import { DiscordMessageCommandFeatureNoonDisabled } from "../classes/discord-message-command-feature-noon-disabled";
 import { DiscordMessageCommandFeatureNoonEnabled } from "../classes/discord-message-command-feature-noon-enabled";
 import { DiscordMessageCommandFeatureNoonFlagEnum } from "../enums/discord-message-command-feature-noon-flag.enum";
 import { DISCORD_MESSAGE_COMMAND_FEATURE_NOON_FLAGS } from "./discord-message-command-feature-noon-flags";
@@ -19,6 +20,12 @@ describe(`DISCORD_MESSAGE_COMMAND_FEATURE_NOON_FLAGS`, (): void => {
             description: `Enable the noon message on this channel. The message will be sent on the ${TimezoneEnum.PARIS} timezone.`,
             name: DiscordMessageCommandFeatureNoonFlagEnum.ENABLED,
             shortcuts: [DiscordMessageCommandFeatureNoonFlagEnum.E],
+          }),
+          new DiscordCommandBooleanFlag({
+            action: new DiscordMessageCommandFeatureNoonDisabled(),
+            description: `Disable the noon message on this channel.`,
+            name: DiscordMessageCommandFeatureNoonFlagEnum.DISABLED,
+            shortcuts: [DiscordMessageCommandFeatureNoonFlagEnum.D],
           }),
         ],
       })
--- src/features/discord/messages/services/command/feature/features/noon/enums/discord-message-command-feature-noon-flag.enum.ts
+++ src/features/discord/messages/services/command/feature/features/noon/enums/discord-message-command-feature-noon-flag.enum.ts
@@ -1,5 +1,10 @@
 export enum DiscordMessageCommandFeatureNoonFlagEnum {
   /**
+   * Shortcut for {@link DISABLED}
+   */
+  D = `d`,
+  DISABLED = `disabled`,
+  /**
    * Shortcut for {@link ENABLED}
    */
   E = `e`,
