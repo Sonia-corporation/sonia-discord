35fd37b4b00a072330b3d5a162266954580551d1
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-11-01 16:58:26 +0100

    chore(help): add help service to contain the common stuff between help messages

---
+++ src/features/discord/messages/services/discord-message-help.service.spec.ts
@@ -0,0 +1,258 @@
+import {
+  MessageEmbedAuthor,
+  MessageEmbedFooter,
+  MessageEmbedThumbnail,
+} from "discord.js";
+import moment from "moment-timezone";
+import { createMock } from "ts-auto-mock";
+import { ColorEnum } from "../../../../enums/color.enum";
+import { IconEnum } from "../../../../enums/icon.enum";
+import { ServiceNameEnum } from "../../../../enums/service-name.enum";
+import { CoreEventService } from "../../../core/services/core-event.service";
+import { DiscordSoniaService } from "../../users/services/discord-sonia.service";
+import { DiscordMessageConfigService } from "./config/discord-message-config.service";
+import { DiscordMessageHelpService } from "./discord-message-help.service";
+
+jest.mock(`../../../logger/services/chalk/chalk.service`);
+
+describe(`DiscordMessageHelpService`, (): void => {
+  let service: DiscordMessageHelpService;
+  let coreEventService: CoreEventService;
+  let discordSoniaService: DiscordSoniaService;
+  let discordMessageConfigService: DiscordMessageConfigService;
+
+  beforeEach((): void => {
+    coreEventService = CoreEventService.getInstance();
+    discordSoniaService = DiscordSoniaService.getInstance();
+    discordMessageConfigService = DiscordMessageConfigService.getInstance();
+  });
+
+  describe(`getInstance()`, (): void => {
+    it(`should create a DiscordMessageHelp service`, (): void => {
+      expect.assertions(1);
+
+      service = DiscordMessageHelpService.getInstance();
+
+      expect(service).toStrictEqual(expect.any(DiscordMessageHelpService));
+    });
+
+    it(`should return the created DiscordMessageHelp service`, (): void => {
+      expect.assertions(1);
+
+      const result = DiscordMessageHelpService.getInstance();
+
+      expect(result).toStrictEqual(service);
+    });
+  });
+
+  describe(`constructor()`, (): void => {
+    let coreEventServiceNotifyServiceCreatedSpy: jest.SpyInstance;
+
+    beforeEach((): void => {
+      coreEventServiceNotifyServiceCreatedSpy = jest
+        .spyOn(coreEventService, `notifyServiceCreated`)
+        .mockImplementation();
+    });
+
+    it(`should notify the DiscordMessageHelp service creation`, (): void => {
+      expect.assertions(2);
+
+      service = new DiscordMessageHelpService();
+
+      expect(coreEventServiceNotifyServiceCreatedSpy).toHaveBeenCalledTimes(1);
+      expect(coreEventServiceNotifyServiceCreatedSpy).toHaveBeenCalledWith(
+        ServiceNameEnum.DISCORD_MESSAGE_HELP_SERVICE
+      );
+    });
+  });
+
+  describe(`getMessageResponse()`, (): void => {
+    let discordSoniaServiceGetCorporationMessageEmbedAuthorSpy: jest.SpyInstance;
+    let discordMessageConfigServiceGetMessageCommandHelpImageColorSpy: jest.SpyInstance;
+    let discordSoniaServiceGetImageUrlSpy: jest.SpyInstance;
+    let discordMessageConfigServiceGetMessageCommandHelpImageUrlSpy: jest.SpyInstance;
+
+    beforeEach((): void => {
+      service = new DiscordMessageHelpService();
+
+      discordSoniaServiceGetCorporationMessageEmbedAuthorSpy = jest.spyOn(
+        discordSoniaService,
+        `getCorporationMessageEmbedAuthor`
+      );
+      discordMessageConfigServiceGetMessageCommandHelpImageColorSpy = jest.spyOn(
+        discordMessageConfigService,
+        `getMessageCommandHelpImageColor`
+      );
+      discordSoniaServiceGetImageUrlSpy = jest.spyOn(
+        discordSoniaService,
+        `getImageUrl`
+      );
+      discordMessageConfigServiceGetMessageCommandHelpImageUrlSpy = jest.spyOn(
+        discordMessageConfigService,
+        `getMessageCommandHelpImageUrl`
+      );
+    });
+
+    it(`should return a Discord message response embed with an author`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+      const messageEmbedAuthor: MessageEmbedAuthor = createMock<
+        MessageEmbedAuthor
+      >();
+      discordSoniaServiceGetCorporationMessageEmbedAuthorSpy.mockReturnValue(
+        messageEmbedAuthor
+      );
+
+      const result = await service.getMessageResponse();
+
+      expect(result.options.embed?.author).toStrictEqual(messageEmbedAuthor);
+    });
+
+    it(`should return a Discord message response embed with a color`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+      discordMessageConfigServiceGetMessageCommandHelpImageColorSpy.mockReturnValue(
+        ColorEnum.CANDY
+      );
+
+      const result = await service.getMessageResponse();
+
+      expect(result.options.embed?.color).toStrictEqual(ColorEnum.CANDY);
+    });
+
+    it(`should return a Discord message response embed without a description`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+
+      const result = await service.getMessageResponse();
+
+      expect(result.options.embed?.description).toBeUndefined();
+    });
+
+    it(`should return a Discord message response embed without fields`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+
+      const result = await service.getMessageResponse();
+
+      expect(result.options.embed?.fields).toBeUndefined();
+    });
+
+    it(`should return a Discord message response embed with a footer containing an icon and a text`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+      discordSoniaServiceGetImageUrlSpy.mockReturnValue(`dummy-image-url`);
+
+      const result = await service.getMessageResponse();
+
+      expect(result.options.embed?.footer).toStrictEqual({
+        iconURL: `dummy-image-url`,
+        text: `At your service`,
+      } as MessageEmbedFooter);
+    });
+
+    describe(`when the Sonia image url is null`, (): void => {
+      beforeEach((): void => {
+        discordSoniaServiceGetImageUrlSpy.mockReturnValue(null);
+      });
+
+      it(`should return a Discord message response embed with a footer but without an icon`, async (): Promise<
+        void
+      > => {
+        expect.assertions(1);
+
+        const result = await service.getMessageResponse();
+
+        expect(result.options.embed?.footer).toStrictEqual({
+          iconURL: undefined,
+          text: `At your service`,
+        } as MessageEmbedFooter);
+      });
+    });
+
+    describe(`when the Sonia image url is "image-url"`, (): void => {
+      beforeEach((): void => {
+        discordSoniaServiceGetImageUrlSpy.mockReturnValue(`image-url`);
+      });
+
+      it(`should return a Discord message response embed with a footer containing an icon and a text`, async (): Promise<
+        void
+      > => {
+        expect.assertions(1);
+
+        const result = await service.getMessageResponse();
+
+        expect(result.options.embed?.footer).toStrictEqual({
+          iconURL: `image-url`,
+          text: `At your service`,
+        } as MessageEmbedFooter);
+      });
+    });
+
+    it(`should return a Discord message response embed with a thumbnail`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+      discordMessageConfigServiceGetMessageCommandHelpImageUrlSpy.mockReturnValue(
+        IconEnum.ARTIFICIAL_INTELLIGENCE
+      );
+
+      const result = await service.getMessageResponse();
+
+      expect(result.options.embed?.thumbnail).toStrictEqual({
+        url: IconEnum.ARTIFICIAL_INTELLIGENCE,
+      } as MessageEmbedThumbnail);
+    });
+
+    it(`should return a Discord message response embed with a timestamp`, async (): Promise<
+      void
+    > => {
+      expect.assertions(2);
+
+      const result = await service.getMessageResponse();
+
+      expect(moment(result.options.embed?.timestamp).isValid()).toStrictEqual(
+        true
+      );
+
+      expect(moment(result.options.embed?.timestamp).fromNow()).toStrictEqual(
+        `a few seconds ago`
+      );
+    });
+
+    it(`should return a Discord message response embed without a title`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+
+      const result = await service.getMessageResponse();
+
+      expect(result.options.embed?.title).toBeUndefined();
+    });
+
+    it(`should return a Discord message response not split`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+
+      const result = await service.getMessageResponse();
+
+      expect(result.options.split).toStrictEqual(false);
+    });
+
+    it(`should return a Discord message response without a response text`, async (): Promise<
+      void
+    > => {
+      expect.assertions(1);
+
+      const result = await service.getMessageResponse();
+
+      expect(result.response).toStrictEqual(``);
+    });
+  });
+});
---
+++ src/features/discord/messages/services/discord-message-help.service.ts
@@ -0,0 +1,78 @@
+import {
+  MessageEmbedAuthor,
+  MessageEmbedFooter,
+  MessageEmbedOptions,
+  MessageEmbedThumbnail,
+} from "discord.js";
+import _ from "lodash";
+import moment from "moment-timezone";
+import { AbstractService } from "../../../../classes/services/abstract.service";
+import { ServiceNameEnum } from "../../../../enums/service-name.enum";
+import { DiscordSoniaService } from "../../users/services/discord-sonia.service";
+import { IDiscordMessageResponse } from "../interfaces/discord-message-response";
+import { DiscordMessageConfigService } from "./config/discord-message-config.service";
+
+export class DiscordMessageHelpService extends AbstractService {
+  private static _instance: DiscordMessageHelpService;
+
+  public static getInstance(): DiscordMessageHelpService {
+    if (_.isNil(DiscordMessageHelpService._instance)) {
+      DiscordMessageHelpService._instance = new DiscordMessageHelpService();
+    }
+
+    return DiscordMessageHelpService._instance;
+  }
+
+  public constructor() {
+    super(ServiceNameEnum.DISCORD_MESSAGE_HELP_SERVICE);
+  }
+
+  public getMessageResponse(): Promise<IDiscordMessageResponse> {
+    return Promise.resolve({
+      options: {
+        embed: this._getMessageEmbed(),
+        split: false,
+      },
+      response: ``,
+    });
+  }
+
+  private _getMessageEmbed(): MessageEmbedOptions {
+    return {
+      author: this._getMessageEmbedAuthor(),
+      color: this._getMessageEmbedColor(),
+      footer: this._getMessageEmbedFooter(),
+      thumbnail: this._getMessageEmbedThumbnail(),
+      timestamp: this._getMessageEmbedTimestamp(),
+    };
+  }
+
+  private _getMessageEmbedAuthor(): MessageEmbedAuthor {
+    return DiscordSoniaService.getInstance().getCorporationMessageEmbedAuthor();
+  }
+
+  private _getMessageEmbedColor(): number {
+    return DiscordMessageConfigService.getInstance().getMessageCommandHelpImageColor();
+  }
+
+  private _getMessageEmbedFooter(): MessageEmbedFooter {
+    const soniaImageUrl:
+      | string
+      | null = DiscordSoniaService.getInstance().getImageUrl();
+
+    return {
+      iconURL: soniaImageUrl ?? undefined,
+      text: `At your service`,
+    };
+  }
+
+  private _getMessageEmbedThumbnail(): MessageEmbedThumbnail {
+    return {
+      url: DiscordMessageConfigService.getInstance().getMessageCommandHelpImageUrl(),
+    };
+  }
+
+  private _getMessageEmbedTimestamp(): Date {
+    return moment().toDate();
+  }
+}
