b1daf579fab92d1f9a6da5ce6a9ab9be3aaa97c4
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-11-21 10:46:01 +0100

    feat(noon): add status flag

--- src/enums/class-name.enum.ts
+++ src/enums/class-name.enum.ts
@@ -3,4 +3,5 @@ export enum ClassNameEnum {
   DISCORD_MESSAGE_COMMAND_FEATURE_NOON_ENABLED = `DiscordMessageCommandFeatureNoonEnabled`,
   DISCORD_MESSAGE_COMMAND_FEATURE_NOON_HELP = `DiscordMessageCommandFeatureNoonHelp`,
   DISCORD_MESSAGE_COMMAND_FEATURE_NOON_HUMANIZE = `DiscordMessageCommandFeatureNoonHumanize`,
+  DISCORD_MESSAGE_COMMAND_FEATURE_NOON_STATUS = `DiscordMessageCommandFeatureNoonStatus`,
 }
