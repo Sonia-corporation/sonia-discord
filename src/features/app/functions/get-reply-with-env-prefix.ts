ed8690a7f4bc8d57dc47e202ed1428790855f09d
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-11-22 22:58:37 +0100

    chore(dev): add new function to set as prefix the dev user

---
+++ src/features/app/functions/get-reply-with-env-prefix.ts
@@ -0,0 +1,16 @@
+import { addDiscordDevPrefix } from '../../discord/functions/dev-prefix/add-discord-dev-prefix';
+import { ProfileConfigService } from '../../profile/services/config/profile-config.service';
+import { AppConfigService } from '../services/config/app-config.service';
+
+export function getReplyWithEnvPrefix(response: Readonly<string>): string {
+  if (!AppConfigService.getInstance().isProduction()) {
+    return addDiscordDevPrefix({
+      asMention: true,
+      discordId: ProfileConfigService.getInstance().getDiscordId(),
+      message: response,
+      nickname: ProfileConfigService.getInstance().getNickname(),
+    });
+  }
+
+  return response;
+}
