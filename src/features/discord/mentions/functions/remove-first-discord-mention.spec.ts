136f862ce4ab0de84e7fd995df643899944268f3
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-11-22 22:28:02 +0100

    chore(mention): add new function to remove the first mention from a message

---
+++ src/features/discord/mentions/functions/remove-first-discord-mention.spec.ts
@@ -0,0 +1,75 @@
+import { removeFirstDiscordMention } from './remove-first-discord-mention';
+
+describe(`removeFirstDiscordMention()`, (): void => {
+  let message: string;
+
+  describe(`when the message is empty`, (): void => {
+    beforeEach((): void => {
+      message = ``;
+    });
+
+    it(`should return an empty message`, (): void => {
+      expect.assertions(1);
+
+      const result = removeFirstDiscordMention(message);
+
+      expect(result).toStrictEqual(``);
+    });
+  });
+
+  describe(`when the message does not contain a mention`, (): void => {
+    beforeEach((): void => {
+      message = `dummy message`;
+    });
+
+    it(`should return the same message`, (): void => {
+      expect.assertions(1);
+
+      const result = removeFirstDiscordMention(message);
+
+      expect(result).toStrictEqual(`dummy message`);
+    });
+  });
+
+  describe(`when the message contains a mention`, (): void => {
+    beforeEach((): void => {
+      message = `dummy message with <@!123> mentioned`;
+    });
+
+    it(`should return the message without the mention`, (): void => {
+      expect.assertions(1);
+
+      const result = removeFirstDiscordMention(message);
+
+      expect(result).toStrictEqual(`dummy message with  mentioned`);
+    });
+  });
+
+  describe(`when the message contains two identical mentions`, (): void => {
+    beforeEach((): void => {
+      message = `dummy message with <@!123> mentioned and again here <@!123>`;
+    });
+
+    it(`should return the message without the first mention`, (): void => {
+      expect.assertions(1);
+
+      const result = removeFirstDiscordMention(message);
+
+      expect(result).toStrictEqual(`dummy message with  mentioned and again here <@!123>`);
+    });
+  });
+
+  describe(`when the message contains two different mentions`, (): void => {
+    beforeEach((): void => {
+      message = `dummy message with <@!123> mentioned and again here <@!456>`;
+    });
+
+    it(`should return the message without the first mention`, (): void => {
+      expect.assertions(1);
+
+      const result = removeFirstDiscordMention(message);
+
+      expect(result).toStrictEqual(`dummy message with  mentioned and again here <@!456>`);
+    });
+  });
+});
---
+++ src/features/discord/mentions/functions/remove-first-discord-mention.ts
@@ -0,0 +1,15 @@
+import { discordGetMentionRegexp } from './discord-get-mention-regexp';
+import _ from 'lodash';
+import xregexp from 'xregexp';
+
+/**
+ * @description
+ * Remove the first mention from the given message
+ *
+ * @param {Readonly<string>} message The message to alter
+ *
+ * @return {string} The message without the first encountered mention
+ */
+export function removeFirstDiscordMention(message: Readonly<string>): string {
+  return _.trim(xregexp.replace(message, discordGetMentionRegexp(), ``, `one`));
+}
