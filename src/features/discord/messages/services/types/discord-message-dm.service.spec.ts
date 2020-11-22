81082163013df3368827627791f18dcfbb1223c4
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2020-11-22 23:08:19 +0100

    feat(response): respond pong when mentioned with ping in a DM

--- src/features/discord/messages/services/types/discord-message-dm.service.spec.ts
+++ src/features/discord/messages/services/types/discord-message-dm.service.spec.ts
@@ -1,13 +1,18 @@
 import { DiscordMessageDmService } from './discord-message-dm.service';
 import { ServiceNameEnum } from '../../../../../enums/service-name.enum';
 import { CoreEventService } from '../../../../core/services/core-event.service';
+import { DiscordAuthorService } from '../../../users/services/discord-author.service';
+import { IAnyDiscordMessage } from '../../types/any-discord-message';
+import { createMock } from 'ts-auto-mock';
 
 describe(`DiscordMessageDmService`, (): void => {
   let service: DiscordMessageDmService;
   let coreEventService: CoreEventService;
+  let discordAuthorService: DiscordAuthorService;
 
   beforeEach((): void => {
     coreEventService = CoreEventService.getInstance();
+    discordAuthorService = DiscordAuthorService.getInstance();
   });
 
   describe(`getInstance()`, (): void => {
@@ -44,6 +49,32 @@ describe(`DiscordMessageDmService`, (): void => {
 
       expect(coreEventServiceNotifyServiceCreatedSpy).toHaveBeenCalledTimes(1);
       expect(coreEventServiceNotifyServiceCreatedSpy).toHaveBeenCalledWith(ServiceNameEnum.DISCORD_MESSAGE_DM_SERVICE);
+    });
+  });
+
+  describe(`getMessage()`, (): void => {
+    let anyDiscordMessage: IAnyDiscordMessage;
+
+    let discordAuthorServiceIsValidSpy: jest.SpyInstance;
+
+    beforeEach((): void => {
+      service = new DiscordMessageDmService();
+      anyDiscordMessage = createMock<IAnyDiscordMessage>({
+        author: {
+          id: `dummy-author-id`,
+        },
+      });
+
+      discordAuthorServiceIsValidSpy = jest.spyOn(discordAuthorService, `isValid`).mockImplementation();
+    });
+
+    it(`should check if the author of the message is valid`, async (): Promise<void> => {
+      expect.assertions(3);
+
+      await expect(service.getMessage(anyDiscordMessage)).rejects.toThrow(new Error(`Invalid author`));
+
+      expect(discordAuthorServiceIsValidSpy).toHaveBeenCalledTimes(1);
+      expect(discordAuthorServiceIsValidSpy).toHaveBeenCalledWith(anyDiscordMessage.author);
     });
   });
 });
