f7cd372c6aa455a3e75feca2245af7e45e3940e0
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2022-11-06 22:56:08 +0100

    refactor: include thread channel in general channel

--- src/features/discord/channels/services/discord-channel-guild.service.ts
+++ src/features/discord/channels/services/discord-channel-guild.service.ts
@@ -2,7 +2,7 @@ import { AbstractService } from '../../../../classes/services/abstract.service';
 import { ServiceNameEnum } from '../../../../enums/service-name.enum';
 import { isDiscordGuild } from '../../guilds/functions/is-discord-guild';
 import { isDiscordGuildChannel } from '../functions/is-discord-guild-channel';
-import { Guild, GuildChannel } from 'discord.js';
+import { Guild, GuildChannel, ThreadChannel } from 'discord.js';
 import _ from 'lodash';
 
 export class DiscordChannelGuildService extends AbstractService {
@@ -20,17 +20,21 @@ export class DiscordChannelGuildService extends AbstractService {
     super(ServiceNameEnum.DISCORD_CHANNEL_GUILD_SERVICE);
   }
 
-  public isGeneral({ name }: Readonly<GuildChannel>): boolean {
-    return _.isEqual(_.toLower(_.deburr(name)), `general`);
+  public isGeneral({ name }: Readonly<GuildChannel | ThreadChannel>): boolean {
+    if (_.isString(name)) {
+      return _.isEqual(_.toLower(_.deburr(name)), `general`);
+    }
+
+    return false;
   }
 
-  public getPrimary(guild: Readonly<Guild>): GuildChannel | null {
+  public getPrimary(guild: Readonly<Guild>): GuildChannel | ThreadChannel | null {
     if (!isDiscordGuild(guild)) {
       return null;
     }
 
-    const primaryChannel: GuildChannel | undefined = guild.channels.cache.find(
-      (channel: Readonly<GuildChannel>): boolean => this.isGeneral(channel)
+    const primaryChannel: GuildChannel | ThreadChannel | undefined = guild.channels.cache.find(
+      (channel: Readonly<GuildChannel | ThreadChannel>): boolean => this.isGeneral(channel)
     );
 
     if (!isDiscordGuildChannel(primaryChannel)) {
