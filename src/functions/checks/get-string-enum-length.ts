7a760b3ce2b4233842ef8576b96e289ca1824bd8
codefactor-io <support@codefactor.io>
2022-11-19 13:47:34 +0000

    [CodeFactor] Apply fixes to commit 55a2acc

--- src/functions/checks/get-string-enum-length.ts
+++ src/functions/checks/get-string-enum-length.ts
@@ -4,9 +4,7 @@ import _ from 'lodash';
 /**
  * @description
  * Return the length of the given string enumeration
- *
  * @param {IObject<string>} enumeration The enumeration
- *
  * @returns {number} The length of the given enumeration
  */
 export function getStringEnumLength(enumeration: IObject<string>): number {
