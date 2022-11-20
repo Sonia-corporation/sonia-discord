5f64747ee55d73e638b081aca324e6c31886f4b5
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2022-11-20 14:07:21 +0100

    chore: send a warning in the warnings channel when a channel type is not supported

--- src/enums/service-name.enum.spec.ts
+++ src/enums/service-name.enum.spec.ts
@@ -2,10 +2,10 @@ import { ServiceNameEnum } from './service-name.enum';
 import { getEnumLength } from '../functions/checks/get-enum-length';
 
 describe(`ServiceNameEnum`, (): void => {
-  it(`should have 123 members`, (): void => {
+  it(`should have 124 members`, (): void => {
     expect.assertions(1);
 
-    expect(getEnumLength(ServiceNameEnum)).toBe(123);
+    expect(getEnumLength(ServiceNameEnum)).toBe(124);
   });
 
   it(`should have a member "APP_CONFIG_CORE_SERVICE"`, (): void => {
@@ -381,6 +381,14 @@ describe(`ServiceNameEnum`, (): void => {
 
     expect(ServiceNameEnum.DISCORD_MESSAGE_COMMAND_VERIFY_CHANNEL_RIGHT_SERVICE).toBe(
       `DiscordMessageCommandVerifyChannelRightService`
+    );
+  });
+
+  it(`should have a member "DISCORD_MESSAGE_COMMAND_VERIFY_CHANNEL_RIGHT_WARNING_UNSUPPORTED_CHANNEL_TYPE_SERVICE"`, (): void => {
+    expect.assertions(1);
+
+    expect(ServiceNameEnum.DISCORD_MESSAGE_COMMAND_VERIFY_CHANNEL_RIGHT_WARNING_UNSUPPORTED_CHANNEL_TYPE_SERVICE).toBe(
+      `DiscordMessageCommandVerifyChannelRightWarningUnsupportedChannelTypeService`
     );
   });
 
